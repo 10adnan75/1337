695b747fa23a6b654f0dfe672954cf0f3c5692a4
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-03-30 13:26:47 +0530

    Update 14_LongestCommonPrefix.java

--- 14_LongestCommonPrefix.java
+++ 14_LongestCommonPrefix.java
@@ -3,7 +3,7 @@ class Solution {
         String longest = new String();
         if(strs.length<=0) return longest;
         Arrays.sort(strs);
-        for(int i=0; i<Math.min(strs[0].length(), strs[strs.length-1].length());i++) {
+        for(int i=0; i<Math.min(strs[0].length(), strs[strs.length-1].length()); i++) {
             if (strs[0].charAt(i)!=strs[strs.length-1].charAt(i)) return longest;
             longest += strs[0].charAt(i);
         }
