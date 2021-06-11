69c08a767b24e55ab55c080272d6959c383d1f99
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-06-11 17:47:04 +0530

    Update stone_game_VII.cpp

--- LeetCoding Challenge/stone_game_VII.cpp
+++ LeetCoding Challenge/stone_game_VII.cpp
@@ -9,18 +9,18 @@ class Solution {
                 return dp[i][j][turn];
             }
             if (turn == 1) { //alice turn
-                int x=total_sum-stones[i];
+                int x = total_sum-stones[i];
                 int res1 = stoneGame_helper(stones, 1-turn, x, i+1, j) + x;
-                int y=total_sum-stones[j];
-                int res2 = stoneGame_helper(stones, 1-turn, y,  i, j-1) + y;
-                return dp[i][j][turn]=max(res1, res2);
+                int y = total_sum-stones[j];
+                int res2 = stoneGame_helper(stones, 1-turn, y, i, j-1) + y;
+                return dp[i][j][turn] = max(res1, res2);
             }
             else if (turn == 0) { //bob turn
-                int x=total_sum-stones[i];
-                int res1 = stoneGame_helper(stones, 1-turn, x,  i+1, j)-x;
-                int y=total_sum-stones[j];
-                int res2 = stoneGame_helper(stones, 1-turn, y, i,j-1)-y;        
-                return dp[i][j][turn]=min(res1, res2);
+                int x = total_sum - stones[i];
+                int res1 = stoneGame_helper(stones, 1-turn, x, i+1, j) - x;
+                int y = total_sum - stones[j];
+                int res2 = stoneGame_helper(stones, 1-turn, y, i, j-1) - y;        
+                return dp[i][j][turn] = min(res1, res2);
             }
             return 0;
         }
