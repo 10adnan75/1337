a6624818c984adfe13692ecc8687af90d4bc1066
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-06-27 20:13:32 +0530

    Update candy.py

--- LeetCoding Challenge/candy.py
+++ LeetCoding Challenge/candy.py
@@ -10,8 +10,8 @@ class Solution:
             if ratings[i] > ratings[i-1] and candies[i] <= candies[i-1]:
                 candies[i] = candies[i-1] + 1
         
-        for i in range(len(ratings) - 2, -1, -1):
+        for i in range(len(ratings)-2, -1, -1):
             if ratings[i] > ratings[i+1] and candies[i] <= candies[i+1]:
-                candies[i] = candies[i+1]+1
+                candies[i] = candies[i+1] + 1
         
         return sum(candies)
