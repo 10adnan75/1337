5ac09f371eba5549fe8bf2476d39f8a88037a830
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-09-29 16:45:00 +0530

    Create B.java

---
+++ Study Plan/Algorithm/I/Day 10/B.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        // Using aux array - Runtime: 0 ms, Memory Usage: 38.3 MB
+        /* int[] a = new int[5001];
+        int size = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            a[size++] = temp.val;
+            temp = temp.next;
+        }
+        temp = head;
+        for (int i=size-1; i>=0; i--) {
+            temp.val = a[i];
+            temp = temp.next;
+        }
+        return head; */
+        
+        // Recursion / Backtracking - Runtime: 0 ms, Memory Usage: 39 MB
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode rest = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return rest;
+    }
+}
