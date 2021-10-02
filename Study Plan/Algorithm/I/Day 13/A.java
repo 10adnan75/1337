d05f1d743372b654ff9860b955e20e98bc2a192d
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-10-02 20:53:56 +0530

    Create A.java

---
+++ Study Plan/Algorithm/I/Day 13/A.java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    public boolean isPowerOfTwo(int n) {
+        
+        // Naive approach - TLE
+        /* while (n % 2 == 0) {
+            n /= 2;
+        }
+        return (n == 1); */
+        
+        // Recursive approach - Runtime: 2 ms, Memory Usage: 38.2 MB
+        /* if (n <= 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        if (n % 2 != 0) {
+            return false;
+        }
+        return isPowerOfTwo(n/2); */
+        
+        // Optimal approach - Runtime: 1 ms, Memory Usage: 38.4 MB
+        return (n > 0) && (n & (n-1)) == 0; 
+    }
+}
