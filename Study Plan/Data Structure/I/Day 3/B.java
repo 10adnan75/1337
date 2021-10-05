a6e7a1344bb8dc87a9f0201f44544ef10f76ac10
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-10-05 23:02:18 +0530

    Create B.java

---
+++ Study Plan/Data Structure/I/Day 3/B.java
@@ -0,0 +1,50 @@
+class Solution {
+    
+    public int maxProfit(int[] prices) {
+        
+        // One pass - Runtime: 5 ms, Memory: 105.3 MB
+        /* if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+        int minSoFar = prices[0];
+        int ans = 0;
+        for (int i=1; i<prices.length; i++) {
+            ans = Math.max(ans, prices[i]-minSoFar);
+            minSoFar = Math.min(minSoFar, prices[i]); 
+        }
+        return ans; */
+        
+        // Dynamic Programming - Runtime: 7 ms, Memory: 107.6 MB
+        /* if (prices.length == 0) {
+            return 0;
+        }
+        int[] profit = new int[prices.length];
+        for (int i=0; i<profit.length; i++) {
+            profit[i] = 0;
+        }
+        int minSoFar = prices[0];
+        for (int i=1; i<prices.length; i++) {
+            if (prices[i] < minSoFar) {
+                minSoFar = prices[i];
+            }
+            profit[i] = Math.max(profit[i-1], prices[i]-minSoFar);
+        }
+        return profit[prices.length-1]; */
+      
+        // My approach - Runtime: 3 ms, Memory: 105.2 MB
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int min = prices[0], max = Integer.MIN_VALUE;
+        for (int i=0; i<prices.length; i++) {
+            int money = prices[i] - min;
+            if (money > max) {
+                max = money;
+            }
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+        }
+        return max;
+    }
+}
