b95e51f9b188de24274dfcf7b949333f75d8fdeb
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-10-07 14:32:43 +0530

    Update A.java

--- Study Plan/Data Structure/I/Day 5/A.java
+++ Study Plan/Data Structure/I/Day 5/A.java
@@ -1,5 +1,38 @@
 class Solution {
+    
     public boolean isValidSudoku(char[][] board) {
+        
+        // HashMap - Runtime: 2ms, Memory: 38.9 MB
+        /* if (board == null || board.length == 0) {
+            return false;
+        }
+        HashMap<Integer, Integer>[] rows = new HashMap[9];
+        HashMap<Integer, Integer>[] cols = new HashMap[9];
+        HashMap<Integer, Integer>[] boxes = new HashMap[9];
+        for (int i=0; i<9; i++) {
+            rows[i] = new HashMap<>();
+            cols[i] = new HashMap<>();
+            boxes[i] = new HashMap<>();
+        }
+        int number = 0;
+        for (int i=0; i<9; i++) {
+            for (int j=0; j<9; j++) {
+                char n = board[i][j];
+                if (n != '.') {
+                    number = (int) n;
+                    int boxIdx = (i/3)*3 + j/3;
+                    rows[i].put(number, rows[i].getOrDefault(number, 0)+1);
+                    cols[j].put(number, cols[j].getOrDefault(number, 0)+1);
+                    boxes[boxIdx].put(number, boxes[boxIdx].getOrDefault(number, 0)+1);
+                    if (rows[i].get(number)>1 || cols[j].get(number)>1 || boxes[boxIdx].get(number)>1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true; */
+        
+        // HashSet - Runtime: 2ms, Memory: 38.8 MB
         for (int i=0; i<9; i++) {
             Set<Character> rows = new HashSet<>();
             Set<Character> columns = new HashSet<>();
