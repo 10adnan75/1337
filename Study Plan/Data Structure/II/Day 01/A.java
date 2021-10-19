0a65816d44053621cc4704c54dda62aa600c655c
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-10-20 00:08:17 +0530

    Update A.java

--- Study Plan/Data Structure/II/Day 01/A.java
+++ Study Plan/Data Structure/II/Day 01/A.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int singleNo = nums[0];
+        
+        for(int k=1; k<nums.length; k++) {
+            singleNo ^= nums[k];
+        }
+        
+        return singleNo;
+    }
+}
