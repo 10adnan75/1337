953349cf40e13d293177ac5e7e0c676e31837304
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-10-25 10:10:25 +0530

    Create B.java

---
+++ Study Plan/Data Structure/II/Day 04/B.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        int ans = 0, curr = intervals[0][1];
+        for (int i=1; i<intervals.length; i++) {
+            if (intervals[i][0] >= curr) {
+                curr = intervals[i][1];
+            } else {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
