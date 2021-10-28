0ff1ac4c7d388632711f015960f70a1d9506bd65
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-10-28 21:25:55 +0530

    Update 2_AddTwoNumbers.java

--- 2_AddTwoNumbers.java
+++ 2_AddTwoNumbers.java
@@ -7,8 +7,8 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
  
+ // Runtime: 4 ms, Memory: 44.9 MB
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode ans = new ListNode(0);
@@ -45,5 +45,32 @@ class Solution {
         }
         
         return ans.next;
+    }
+} */
+
+// Runtime: 2 ms, Memory: 39.6 MB
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry != 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummy.next;
     }
 }
