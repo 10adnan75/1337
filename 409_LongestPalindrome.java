247be585f0cb5742ac60ddfe2277b562ec96b225
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-10-28 19:09:46 +0530

    Create 409_LongestPalindrome.java

---
+++ 409_LongestPalindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int count = 0;
+        HashSet<Character> data = new HashSet<>();
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (data.contains(c)) {
+                count += 2;
+                data.remove(c);
+            } else {
+                data.add(c);
+            }
+        }
+        if (data.size() > 0) {
+            count++;
+        }
+        return count;
+    }
+}
