6a1a18b1acf9cf7763b955f02df1dcfe39ca728d
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-11-20 15:36:03 +0530

    Create A.java

---
+++ Study Plan/Data Structure/II/Day 11/A.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // Runtime - 7 ms, Memory - 42.8 MB
+        /* Set<ListNode> set = new HashSet<>();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null) {
+            if (set.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;*/
+      
+        // Runtime - 1 ms, Memory - 41.9 MB
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA; 
+    }
+}
