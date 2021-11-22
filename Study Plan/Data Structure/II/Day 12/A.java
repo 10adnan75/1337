eec68812b6e5f920656b527d85a07fef79b81685
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-11-22 13:20:19 +0530

    Create A.java

---
+++ Study Plan/Data Structure/II/Day 12/A.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = dummy;
+        while (curr.next != null && curr.next.next != null) {
+            ListNode first = curr.next, second = curr.next.next;
+            first.next = second.next;
+            curr.next = second;
+            curr.next.next = first;
+            curr = curr.next.next;
+        }
+        return dummy.next;
+    }
+}
