373c3bb0fef0987878db39f7945b00e4e032a643
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-11-24 19:24:25 +0530

    Create 1249_MinimumRemovetoMakeValidParentheses.java

---
+++ 1249_MinimumRemovetoMakeValidParentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Set<Integer> set = new HashSet<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            }
+            if (c == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    set.add(i);
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            set.add(stack.pop());
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i=0; i<s.length(); i++) {
+            if (!set.contains(i)) {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
