0675633c0b95d7624db2ba418d6dec2da5701934
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-11-28 20:11:16 +0530

    Create A.java

---
+++ Study Plan/Data Structure/II/A.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode p = root;
+        int ans = 0;
+        while (!stack.isEmpty() || p != null) {
+            if (p != null) {
+                stack.push(p);
+                p = p.left;
+            } else {
+                TreeNode t = stack.pop();
+                k--;
+                if (k == 0) {
+                    ans = t.val;
+                }
+                p = t.right;
+            }
+        }
+        return ans;
+    }
+}
