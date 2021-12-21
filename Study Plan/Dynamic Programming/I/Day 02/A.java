2aa59f2acac01527bc8f3e9e413ec626648bc83c
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-12-21 13:25:57 +0530

    Update A.java

--- Study Plan/Dynamic Programming/I/Day 02/A.java
+++ Study Plan/Dynamic Programming/I/Day 02/A.java
@@ -1,1 +1,28 @@
-
+class Solution {
+    
+    int[] cache = new int[46];
+    
+    public int climbStairs(int n) {
+        // Enhanced recursion - Runtime: 0 ms, Memory Usage: 37.7 MB
+        /* if (n < 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        int step = cache[n];
+        if (step != 0) {
+            return step;
+        }
+        step = climbStairs(n-1) + climbStairs(n-2);
+        cache[n] = step;
+        return step; */
+        
+        // Optimal memoization - Runtime: 0 ms, Memory Usage: 35.6 MB
+        cache[0] = 1; cache[1] = 1;
+        for (int i=2; i<=n; i++) {
+            cache[i] = cache[i-1] + cache[i-2];
+        }
+        return cache[n];
+    }
+}
