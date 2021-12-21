e8d5366416b7ca9dc47625b2c08f64bd55ebe1a3
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-12-21 12:29:36 +0530

    Update A.java

--- Study Plan/Dynamic Programming/I/Day 01/A.java
+++ Study Plan/Dynamic Programming/I/Day 01/A.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int[] series = new int[n+1];
+        series[0] = 0;
+        series[1] = 1;
+        for (int i=2; i<=n; i++) {
+            series[i] = series[i-1] + series[i-2];
+        }
+        return series[n];
+    }
+}
