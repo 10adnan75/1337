7cfda29b543fe2e8efaf1fe4cc3e649c1018eb5c
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2022-02-20 22:23:23 +0530

    Create B.java

---
+++ Study Plan/Dynamic Programming/I/Day 20/B.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for (int i=0; i<coins.length; i++) {
+            for (int j=1; j<=amount; j++) {
+                if (j-coins[i] >= 0) {
+                    dp[j] += dp[j-coins[i]];
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
