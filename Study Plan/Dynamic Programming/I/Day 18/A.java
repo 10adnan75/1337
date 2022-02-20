f116e1e3c5177c96001a36bd4b8d86ab4291d310
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2022-02-20 22:14:15 +0530

    Update A.java

--- Study Plan/Dynamic Programming/I/Day 18/A.java
+++ Study Plan/Dynamic Programming/I/Day 18/A.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int size = nums.length;
+        if (size <= 0) {
+            return 0;
+        }
+        int[] result = new int[size];
+        int max = 1;
+        for (int i=0; i<nums.length; i++) {
+            result[i] = 1;
+            for (int j=0; j<i; j++) {
+                if (nums[j] < nums[i]) {
+                    result[i] = result[i] < result[j] + 1 ? result[j] + 1 : result[i];
+                }
+            }
+            if (max < result[i]) {
+                max = result[i];
+            }
+        }
+        return max;
+    }
+}
