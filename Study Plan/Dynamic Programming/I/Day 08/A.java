87f966e3bff0c6243473a8588d81d40ac6d9f46c
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2022-02-20 21:29:41 +0530

    Update A.java

--- Study Plan/Dynamic Programming/I/Day 08/A.java
+++ Study Plan/Dynamic Programming/I/Day 08/A.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+        int len = prices.length;
+        int[] buy = new int[len+1], sell = new int[len+1];
+        buy[1] = -prices[0];
+        for (int i=2; i<=len; i++) {
+            buy[i] = Math.max(sell[i-2]-prices[i-1], buy[i-1]);
+            sell[i] = Math.max(sell[i-1], buy[i-1]+prices[i-1]);
+        }
+        return sell[len];
+    }
+}
