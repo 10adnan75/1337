f7acfb2c151f92728cd9412d58f4af2754bbc6cb
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2022-02-20 22:19:18 +0530

    Update A.java

--- Study Plan/Dynamic Programming/I/Day 19/A.java
+++ Study Plan/Dynamic Programming/I/Day 19/A.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() < 1) {
+            return true;
+        }
+        int idx = 0;
+        for (char c : t.toCharArray()) {
+            if (s.charAt(idx) == c) {
+                idx += 1;
+            }
+            if (idx == s.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
