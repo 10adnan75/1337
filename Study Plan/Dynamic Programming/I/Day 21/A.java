0d6e2aac147da941a793d3ad36aeffb0211eaa80
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2022-02-20 22:25:09 +0530

    Update A.java

--- Study Plan/Dynamic Programming/I/Day 21/A.java
+++ Study Plan/Dynamic Programming/I/Day 21/A.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] count = new int[target+1];
+        count[0] = 1;
+        for (int i=1; i<=target; i++) {
+            for (int num : nums) {
+                if (i >= num) {
+                    count[i] += count[i-num];
+                }
+            }
+        }
+        return count[target];
+    }
+}
