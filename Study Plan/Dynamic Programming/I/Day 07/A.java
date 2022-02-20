7efda3005c09dc371e26a83b557f1e44c56b809a
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2022-02-20 21:20:59 +0530

    Update A.java

--- Study Plan/Dynamic Programming/I/Day 07/A.java
+++ Study Plan/Dynamic Programming/I/Day 07/A.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        int store[] = new int[values.length];
+        int max = 0;
+        for (int i=0; i<values.length-1; i++) {
+            max = Math.max(max, values[i]+i);
+            store[i] = max;
+        }
+        int res = Integer.MIN_VALUE;
+        for (int i=values.length-1; i>=1; i--) {
+            res = Math.max(res, values[i]-i+store[i-1]);
+        }
+        return res;
+    }
+}
