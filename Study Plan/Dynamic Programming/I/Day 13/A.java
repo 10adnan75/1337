a7c1b23ff43a530d9584821cef61ef3754f88cd9
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2022-02-20 21:46:54 +0530

    Update A.java

--- Study Plan/Dynamic Programming/I/Day 13/A.java
+++ Study Plan/Dynamic Programming/I/Day 13/A.java
@@ -1,1 +1,24 @@
-
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        for (int row=n-2; row>=0; row--) {
+            for (int col=0; col<n; col++) {
+                int best = matrix[row+1][col];
+                if (col > 0) {
+                    best = Math.min(best, matrix[row+1][col-1]);
+                }
+                if (col < n-1) {
+                    best = Math.min(matrix[row+1][col+1], best);
+                }
+                matrix[row][col] += best;
+            }
+        }
+        int res = Integer.MAX_VALUE;
+        for (int x : matrix[0]) {
+            if (x < res) {
+                res = x;
+            }
+        }
+        return res;
+    }
+}
