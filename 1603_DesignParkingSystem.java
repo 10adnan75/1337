79838939baa049300caef60d899ba8315f25b8bc
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-04-19 11:25:30 +0530

    Create 1603_DesignParkingSystem.java

---
+++ 1603_DesignParkingSystem.java
@@ -0,0 +1,24 @@
+class ParkingSystem {
+    public int big, medium, small;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+    
+    public boolean addCar(int carType) {
+        switch (carType) {
+            case 1: return this.big-- > 0;
+            case 2: return this.medium-- > 0;
+            case 3: return this.small-- > 0;
+        }
+        return true;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
