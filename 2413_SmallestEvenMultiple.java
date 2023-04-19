5814109df2d83764d7995a1cf451c7d1e7c55bbb
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-04-19 15:16:42 +0530

    Update 2413_SmallestEvenMultiple.java

--- 2413_SmallestEvenMultiple.java
+++ 2413_SmallestEvenMultiple.java
@@ -1,3 +1,16 @@
+// Runtime: 0 ms, Beats 100%
+// Memory: 39.7 MB, Beats 28.18%
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if ((n & 1) == 1) {
+            return n * 2;
+        }
+        return n;
+    }
+}
+
+// Runtime: 0 ms, Beats 100%
+// Memory: 39.4 MB, Beats 71.44%
 class Solution {
     public int smallestEvenMultiple(int n) {
         int sem = 1;
