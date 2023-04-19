c4d61358f0a35a1432b5e764ed0daceba3740e51
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-04-19 13:53:50 +0530

    Create 1108_DefanginganIPAddress.java

---
+++ 1108_DefanginganIPAddress.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder ans = new StringBuilder();
+        for (int i=0; i<address.length(); i++) {
+            if (address.charAt(i) == '.') {
+                ans.append("[.]");
+                continue;
+            }
+            ans.append(address.charAt(i));
+        }
+        return ans.toString();
+    }
+}
