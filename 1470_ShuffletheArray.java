03d8f30f24f2c51f07052eee8437b320009c2a37
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-04-19 14:24:07 +0530

    Update 1470_ShuffletheArray.java

--- 1470_ShuffletheArray.java
+++ 1470_ShuffletheArray.java
@@ -1,3 +1,22 @@
+// Runtime: 0 ms, Beats 100%
+// Memory: 43.2 MB, Beats 7.88%
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[n*2];
+        int x = 0, y = n;
+        for (int i=0; i<ans.length; i++) {
+            if (i % 2 == 0) {
+                ans[i] = nums[x++];
+            } else {
+                ans[i] = nums[y++];
+            }
+        }
+        return ans;
+    }
+}
+
+// Runtime: 0 ms, Beats 100%
+// Memory: 43.1 MB, Beats 11.97%
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] ans = new int[n*2];
