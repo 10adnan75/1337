8e94f43f4407409d9f0851ba8099f8f7deac4cd3
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-04-19 15:41:17 +0530

    Update 2114_MaximumNumberofWordsFoundinSentences.java

--- 2114_MaximumNumberofWordsFoundinSentences.java
+++ 2114_MaximumNumberofWordsFoundinSentences.java
@@ -1,3 +1,4 @@
+//Memory: 41.9 MB
 class Solution {
     public int mostWordsFound(String[] sentences) {
         int max = 0;
@@ -6,6 +7,17 @@ class Solution {
             if (words.length > max) {
                 max = words.length;
             }
+        }
+        return max;
+    }
+}
+
+// Memory: 42.5 MB
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int max = 0;
+        for (String s : sentences) {
+            max = Math.max(max, s.split(" ").length);
         }
         return max;
     }
