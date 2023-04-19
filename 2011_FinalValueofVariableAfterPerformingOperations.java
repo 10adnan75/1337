7ed6df2f195ccc8c3c3058d830b77750a8f8ef05
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-04-19 12:12:58 +0530

    Beats 100%

--- 2011_FinalValueofVariableAfterPerformingOperations.java
+++ 2011_FinalValueofVariableAfterPerformingOperations.java
@@ -1,8 +1,23 @@
+/*
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int count = 0, i = 0;
         while (i < operations.length) {
             if ((operations[i].equals("X++")) || (operations[i].equals("++X"))) {
+                count++;
+            } 
+            i++;
+        }
+        return  count * 2 - operations.length;
+    }
+}
+*/
+
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int count = 0, i = 0;
+        while (i < operations.length) {
+            if (operations[i].charAt(1) == '+') {
                 count++;
             } 
             i++;
