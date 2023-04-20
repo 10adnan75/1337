21836a8829a4c3f6af3186884db694eb6b5f1a81
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-04-20 11:47:37 +0530

    Create 2068_CheckWhetherTwoStringsareAlmostEquivalent.java

---
+++ 2068_CheckWhetherTwoStringsareAlmostEquivalent.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        HashMap<Character, Integer> m1 = new HashMap<>();
+        HashMap<Character, Integer> m2 = new HashMap<>();
+        for (int i=0, j=0; i<word1.length(); i++, j++) {
+            char alpha1 = word1.charAt(i), alpha2 = word2.charAt(i);
+            if (m1.containsKey(alpha1)) {
+                m1.put(alpha1, m1.get(alpha1) + 1);
+            } else {
+                m1.put(alpha1, 1);
+            }
+            if (m2.containsKey(alpha2)) {
+                m2.put(alpha2, m2.get(alpha2) + 1);
+            } else {
+                m2.put(alpha2, 1);
+            }
+        }
+        for (Map.Entry m : m1.entrySet()) {
+            char key = (char) m.getKey();
+            if (!m2.containsKey(key)) {
+                m2.put(key, 0);
+            }
+        }
+        
+        for (Map.Entry m : m2.entrySet()) {
+            char key = (char) m.getKey();
+            if (!m1.containsKey(key)) {
+                m1.put(key, 0);
+            }
+        }
+        int value1, value2;
+        for (Map.Entry m : m1.entrySet()) {
+            if (m2.get(m.getKey()) == null) {
+                value1 = 0;
+            } else {
+                value1 = m2.get(m.getKey());
+            }
+            if (m.getValue() == null) {
+                value2 = 0;
+            } else {
+                value2 = (int)m.getValue();
+            }
+            if (Math.abs(value1 - value2) > 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
