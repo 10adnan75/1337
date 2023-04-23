dd3418f24f6ee9440ae3be615ead6ff5b4dfe6e6
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-04-23 21:16:03 +0530

    Create 832_FlippinganImage.java

---
+++ 832_FlippinganImage.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] reverseArray(int[] a) {
+        int j = a.length-1, temp = 0;
+        for (int i=0; i<a.length/2; i++) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            j--;
+        }
+        return a;
+    }
+
+    public int[][] flipAndInvertImage(int[][] image) {
+        int n = image.length;
+        for (int i=0; i<n; i++) {
+            image[i] = reverseArray(image[i]);
+        }
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (image[i][j] == 0) {
+                    image[i][j] = 1;
+                } else {
+                    image[i][j] = 0;
+                }
+            }
+        }
+        return image;
+    }
+}
