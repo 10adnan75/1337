3f7f9550f5376da20b7e0c4f8a129cd09084cb35
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-04-23 18:40:48 +0530

    Update 977_SquareOfASortedArray.java

--- 977_SquareOfASortedArray.java
+++ 977_SquareOfASortedArray.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int left = 0, right = nums.length - 1;
-        int[] arr = new int[nums.length];
+        int lo = 0, hi = nums.length - 1;
+        int[] sortedSquaresArr = new int[hi + 1];
         for (int i=nums.length-1; i>=0; i--) {
-            if (Math.abs(nums[left]) > nums[right]) {
-                arr[i] = nums[left] * nums[left];
-                left++;
+            if (Math.abs(nums[lo]) > Math.abs(nums[hi])) {
+                sortedSquaresArr[i] = nums[lo] * nums[lo];
+                lo++;
             } else {
-                arr[i] = nums[right] * nums[right];
-                right--;
+                sortedSquaresArr[i] = nums[hi] * nums[hi];
+                hi--;
             }
         }
-        return arr;
+        return sortedSquaresArr;
     }
 }
