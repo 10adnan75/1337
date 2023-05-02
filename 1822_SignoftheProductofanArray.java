12181e6336be619235481d072dfebbe6c5803481
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-05-02 14:51:58 +0530

    Update 1822_SignoftheProductofanArray.java

--- 1822_SignoftheProductofanArray.java
+++ 1822_SignoftheProductofanArray.java
@@ -1,5 +1,21 @@
 class Solution {
     public int arraySign(int[] nums) {
+        int sign = 1;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] == 0) {
+                return 0;
+            } 
+            if (nums[i] < 0) {
+                sign = -sign;
+            }
+        }
+        return sign;
+    }
+}
+
+// My approach
+class Solution {
+    public int arraySign(int[] nums) {
         int negCount = 0;
         for (int i=0; i<nums.length; i++) {
             if (nums[i] == 0) {
