560b378790dbf743d5d5d903227945b87aade619
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-05-21 00:42:59 +0530

    Create 225_ImplementStackusingQueues.java

---
+++ 225_ImplementStackusingQueues.java
@@ -0,0 +1,78 @@
+// Using two queues
+class MyStack {
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        while (!q1.isEmpty()) {
+            q2.offer(q1.poll());
+        }
+        q1.offer(x);
+        while (!q2.isEmpty()) {
+            q1.offer(q2.poll());
+        }
+    }
+    
+    public int pop() {
+        return q1.poll();
+    }
+    
+    public int top() {
+        return q1.peek();
+    }
+    
+    public boolean empty() {
+        return q1.size() == 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+
+// Using single queue
+class MyStack {
+    private Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q.offer(x);
+        for (int i=0; i<q.size()-1; i++) {
+            q.offer(q.poll());
+        }
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
