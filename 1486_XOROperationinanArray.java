d1b9c9d40b0fa06cece6bdc76f3f5b485f0d494f
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-06-10 01:21:04 +0530

    Create 1486_XOROperationinanArray.java

---
+++ 1486_XOROperationinanArray.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int xor = 0;
+        for (int i=0; i<n; i++) {
+            xor ^= start + 2 * i;;
+        }
+        return xor;
+    }
+}
