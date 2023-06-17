5bac71557ad9810b22b92b77c10d0e642fbf0d63
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-06-17 20:30:21 +0530

    Create 1688_CountofMatchesinTournament.java

---
+++ 1688_CountofMatchesinTournament.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return (n / 2) + numberOfMatches(n / 2);
+        }
+        return (n / 2) + numberOfMatches(n / 2 + 1);
+    }
+}
