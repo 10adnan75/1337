4c54e01a60545f8480aed7faa5a17c4a2b1e8d00
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-07-08 21:46:57 +0530

    Update 1464_MaximumProductofTwoElementsinanArray.java

--- 1464_MaximumProductofTwoElementsinanArray.java
+++ 1464_MaximumProductofTwoElementsinanArray.java
@@ -1,6 +1,23 @@
+// Sorting, 2 ms, beats 74.47%
 class Solution {
     public int maxProduct(int[] nums) {
         Arrays.sort(nums);
         return (nums[nums.length-1]-1) * (nums[nums.length-2]-1);
+    }
+}
+
+// Max and Second max, 0 ms, beats 100%
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE, sMax = Integer.MIN_VALUE;
+        for (int i : nums) {
+            if (i > max) {
+                sMax = max;
+                max = i;
+            } else if (i > sMax) {
+                sMax = i;
+            }
+        }
+        return --max * --sMax;
     }
 }
