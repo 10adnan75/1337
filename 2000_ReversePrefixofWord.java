82c585429741175be6d582605ae0c943cd666014
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-07-09 15:22:12 +0530

    Create 2000_ReversePrefixofWord.java

---
+++ 2000_ReversePrefixofWord.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        StringBuilder sb = new StringBuilder();
+        int prefix = -1;
+        for (int i=0; i<word.length(); i++) {
+            if (ch == word.charAt(i)) {
+                prefix = i;
+                break;
+            }
+        }
+        if (prefix == -1) {
+            return word;
+        }
+        // sb.append(word.substring(0, prefix+1)).reverse();
+        for (int i=prefix; i>=0; i--) {
+            sb.append(word.charAt(i));
+        }
+        for (int i=prefix+1; i<word.length(); i++) {
+            sb.append(word.charAt(i));
+        }
+        return sb.toString();
+    }
+}
