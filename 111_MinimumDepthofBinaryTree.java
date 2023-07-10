9bf8aabc36b9e7f41b77a703632efbec8382153c
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-07-10 19:53:20 +0530

    Create 111_MinimumDepthofBinaryTree.java

---
+++ 111_MinimumDepthofBinaryTree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int depth = 1;
+        while (!q.isEmpty()) {
+            int N = q.size();
+            while (N-- > 0) {
+                TreeNode node = q.remove();
+                if (node == null) {
+                    continue;
+                }
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                q.add(node.left);
+                q.add(node.right);
+            }
+            depth++;
+        }
+        return -1;
+    }
+}
