0cb0706e59e4b380e1b120317447b0d6335e9778
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-07-18 13:16:11 +0530

    Update 2553_SeparatetheDigitsinanArray.java

--- 2553_SeparatetheDigitsinanArray.java
+++ 2553_SeparatetheDigitsinanArray.java
@@ -1,3 +1,5 @@
+// Runtime: 7 ms, Beats 48.50%
+// Memory: 44.5 MB, Beats 29%
 class Solution {
     public List<Integer> getDigits(String s) {
         List<Integer> l = new ArrayList<>();
@@ -17,5 +19,27 @@ class Solution {
             ans[i] = l.get(i);
         }
         return ans;
+    }
+}
+
+// Runtime: 1 ms, Beats 100%
+// Memory: 43.8 MB, Beats 85.75%
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        int size = 0;
+        for (int num : nums) {
+            while (num != 0) {
+                size++;
+                num /= 10;
+            }
+        }
+        int[] digits = new int[size];
+        for (int i=nums.length-1; i>=0; i--) {
+            while (nums[i] != 0) {
+                digits[--size] = nums[i] % 10;
+                nums[i] /= 10;
+            }
+        }
+        return digits;
     }
 }
