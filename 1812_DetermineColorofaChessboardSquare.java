e0f54d3c6547be38a27442f5c7581f42287f2a45
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-07-27 15:59:01 +0530

    Update 1812_DetermineColorofaChessboardSquare.java

--- 1812_DetermineColorofaChessboardSquare.java
+++ 1812_DetermineColorofaChessboardSquare.java
@@ -1,5 +1,13 @@
+// My code
 class Solution {
     public boolean squareIsWhite(String coordinates) {
         return ((int)(coordinates.charAt(0)-'a' & 1) == 1 && (int)(coordinates.charAt(1)-'0' & 1) == 1) || ((int)(coordinates.charAt(0)-'a' & 1) == 0 && (int)(coordinates.charAt(1)-'0' & 1) == 0);
+    }
+}
+
+// Optimized
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        return ((int)(coordinates.charAt(0)-'a') + (int)(coordinates.charAt(1)-'0') & 1) == 0;
     }
 }
