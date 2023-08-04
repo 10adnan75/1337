274f689a6144d80a35e2d1daf67a7384959e66fc
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-08-04 23:05:10 +0530

    Create 1572_MatrixDiagonalSum.java

---
+++ 1572_MatrixDiagonalSum.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        int n = mat.length;
+        for (int i=0; i<n; i++) {
+            sum += mat[i][i];
+            sum += mat[i][n-i-1];
+        }
+        return (n & 1) == 1 ? sum - mat[n/2][n/2] : sum;
+    }
+}
