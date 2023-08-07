32daefe23a5c698bfbc3bc9b43f073d3abbc467b
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-08-07 14:54:15 +0530

    Update 2138_DivideaStringIntoGroupsofSizek.java

--- 2138_DivideaStringIntoGroupsofSizek.java
+++ 2138_DivideaStringIntoGroupsofSizek.java
@@ -1,3 +1,4 @@
+// 7 ms
 class Solution {
     public String[] divideString(String s, int k, char fill) {
         List<String> ans = new ArrayList<>();
@@ -26,5 +27,39 @@ class Solution {
             groups[i] = ans.get(i);
         }
         return groups;
+    }
+}
+
+// 8 ms
+class Solution {
+    public String[] divideString(String s, int k, char fill) {
+        int size = 0;
+        if (s.length() % k == 0) {
+            size = s.length() / k;
+        } else {
+            size = s.length() / k + 1;
+        }
+        String[] ans = new String[size];
+        int j = 0;
+        for (int i=0; i<size; i++) {
+            boolean f = false;
+            String sb = "";
+            int idx = 0;
+            for (idx=k; idx>0; idx--) {
+                if (j == s.length() - 1) {
+                    f = !f;
+                    sb += s.charAt(j++) + "";
+                    break;
+                }
+                sb += s.charAt(j++) + "";
+            }
+            if (f) {
+                while (idx-- > 1) {
+                    sb += fill;
+                }
+            }
+            ans[i] = sb;
+        }
+        return ans;
     }
 }
