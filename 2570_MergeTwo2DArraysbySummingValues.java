9aa42749e094146214fcd33ce6ab494ca4bf3849
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-08-08 12:17:00 +0530

    Update 2570_MergeTwo2DArraysbySummingValues.java

--- 2570_MergeTwo2DArraysbySummingValues.java
+++ 2570_MergeTwo2DArraysbySummingValues.java
@@ -56,3 +56,30 @@ class Solution {
         return ans;
     }
 }
+
+// Alternate approach
+class Solution {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        List<int[]> list = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i][0] == nums2[j][0]) {
+                nums1[i][1] += nums2[j][1];
+                list.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i][0] < nums2[j][0]) {
+                list.add(nums1[i++]);
+            } else {
+                list.add(nums2[j++]);
+            }
+        }
+        while (i < nums1.length) {
+            list.add(nums1[i++]);
+        }
+        while (j < nums2.length) {
+            list.add(nums2[j++]);
+        }
+        return list.toArray(new int[list.size()][2]);
+    }
+}
