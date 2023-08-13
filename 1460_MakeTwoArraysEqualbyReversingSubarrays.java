aea1d5c2f63d13ed175b54f13c68e395608298cb
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-08-13 17:08:50 +0530

    Create 1460_MakeTwoArraysEqualbyReversingSubarrays.java

---
+++ 1460_MakeTwoArraysEqualbyReversingSubarrays.java
@@ -0,0 +1,33 @@
+// Need to work on this!
+// class Solution {
+//     public boolean canBeEqual(int[] target, int[] arr) {
+//         int[] freq = new int[1001];
+//         for (int i=0; i<target.length; i++) {
+//             freq[target[i]]++;
+//             freq[arr[i]]++;
+//         }
+//         for (int i : freq) {
+//             if ((i & 1) == 1) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        int[] freqTarget = new int[1001];
+        int[] freqArr = new int[1001];
+        for (int i=0; i<target.length; i++) {
+            freqTarget[target[i]]++;
+            freqArr[arr[i]]++;
+        }
+        for (int i=0; i<1001; i++) {
+            if (freqTarget[i] != freqArr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
