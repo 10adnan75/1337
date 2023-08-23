e8c26017c59b72378716c394767e1a288453d781
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-08-23 16:09:31 +0530

    Update 492_ConstructtheRectangle.java
    Fixed the issue 👍🏻

--- 492_ConstructtheRectangle.java
+++ 492_ConstructtheRectangle.java
@@ -1,6 +1,6 @@
 // 24/52 testcases passed
 // TLE at testcase #25 - 9999998
-Class Solution {
+class Solution {
     public int[] constructRectangle(int area) {
         for (int l=(int)Math.sqrt(area); l<=area; l++) {
             for (int w=1; w<=Math.sqrt(area); w++) {
@@ -12,4 +12,15 @@ Class Solution {
         return new int[]{-1, -1};
     }
 }
-         
+
+// Accepted solution
+class Solution {
+    public int[] constructRectangle(int area) {
+        for (int i=(int)Math.sqrt(area); i>0; i--) {
+            if (area % i == 0) {
+                return new int[] {area / i, i};
+            }
+        }
+        return new int[] {area, 1};
+    }
+}
