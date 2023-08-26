dd825bf5acc4fe305f4948a85dddfaf559793033
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-08-27 00:45:50 +0530

    Create 2563_CounttheNumberofFairPairs.java

---
+++ 2563_CounttheNumberofFairPairs.java
@@ -0,0 +1,17 @@
+// Brute force - TLE
+class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        // long c = 0;
+        // for (int i=0; i<nums.length; i++) {
+        //     for (int j=i+1; j<nums.length; j++) {
+        //         long sum = nums[i] + nums[j];
+        //         if (sum <= upper && sum >= lower) {
+        //             c++;
+        //         }
+        //     }
+        // }
+        // return c;
+    }
+}
+
+// Need to work on this!
