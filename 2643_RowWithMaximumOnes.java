4b2280075715d5a4b4e04052520138b1207eecd2
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-08-29 13:31:13 +0530

    Update 2643_RowWithMaximumOnes.java

--- 2643_RowWithMaximumOnes.java
+++ 2643_RowWithMaximumOnes.java
@@ -1,3 +1,4 @@
+// 4 ms, O(N) space
 class Solution {
     public int[] rowAndMaximumOnes(int[][] mat) {
         int[] ans = new int[mat.length];
@@ -19,5 +20,23 @@ class Solution {
             }
         }
         return new int[] {maxIdx, max};
+    }
+}
+
+// 1 ms, O(1) space
+class Solution {
+    public int[] rowAndMaximumOnes(int[][] mat) {
+        int maxCount = 0, maxIndex = 0;
+        for (int i=0; i<mat.length; i++) {
+            int c = 0;
+            for (int j=0; j<mat[0].length; j++) {
+                c += mat[i][j];
+            }
+            if (c > maxCount) {
+                maxCount = c;
+                maxIndex = i;
+            }
+        }
+        return new int[] {maxIndex, maxCount};
     }
 }
