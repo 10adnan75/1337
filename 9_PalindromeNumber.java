7880289c41977cec92376cd9ec0b36ea4cbba4ff
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-09-11 11:52:33 +0530

    Update 9_PalindromeNumber.java

--- 9_PalindromeNumber.java
+++ 9_PalindromeNumber.java
@@ -1,3 +1,5 @@
+// Runtime: 7 ms, Beats 39.23%
+// Memory: 38.6 MB, Beats 100%
 class Solution {
     public int reverse(int num, int sum) {
 		if(num == 0) {
@@ -10,5 +12,20 @@ class Solution {
         if(x < 0) return false;
         else if(x == reverse(x, 0)) return true;
         else return false;
+    }
+}
+
+// Runtime: 6 ms, Beats 54.16%
+// Memory: 42.5 MB, Beats 91.67%
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = x + "";
+        int n = s.length();
+        for (int i=0; i<n/2; i++) {
+            if (s.charAt(i) != s.charAt(n-i-1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
