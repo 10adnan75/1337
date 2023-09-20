81a5723aa75a601e9e200e159f6c2ad3ec33adfc
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-09-21 01:12:13 +0530

    Update 1903_LargestOddNumberinString.java

--- 1903_LargestOddNumberinString.java
+++ 1903_LargestOddNumberinString.java
@@ -1,3 +1,4 @@
+// My code
 class Solution {
     public String largestOddNumber(String num) {
         int idx = 0;
@@ -9,5 +10,17 @@ class Solution {
             }
         }
         return idx == -1 ? "" : num.substring(0, idx+1);
+    }
+}
+
+// Somewhat optimized
+class Solution {
+    public String largestOddNumber(String num) {
+        for (int i=num.length()-1; i>=0; i--) {
+            if (((int)num.charAt(i) & 1) == 1) {
+                return num.substring(0, i+1);
+            }
+        }
+        return "";
     }
 }
