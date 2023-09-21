a26fd969c807652f9efb87aded5521e67f12520a
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-09-21 12:56:10 +0530

    Update 2108_FindFirstPalindromicStringintheArray.java

--- 2108_FindFirstPalindromicStringintheArray.java
+++ 2108_FindFirstPalindromicStringintheArray.java
@@ -1,3 +1,4 @@
+// My code
 class Solution {
     public boolean isPalindrome(String s) {
         String r = "";
@@ -5,6 +6,27 @@ class Solution {
             r += s.charAt(i);
         }
         return r.equals(s);
+    }
+
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+}
+
+// Effecacious approach
+class Solution {
+    public boolean isPalindrome(String s) {
+        for (int i=0, j=s.length()-1; i<s.length() && j>=0; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public String firstPalindrome(String[] words) {
