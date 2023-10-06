fd284852bc16921477ca8c3551dab441ce7f6186
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-10-06 12:23:56 +0530

    Update 2405_OptimalPartitionofString.java

--- 2405_OptimalPartitionofString.java
+++ 2405_OptimalPartitionofString.java
@@ -1,3 +1,4 @@
+// Hash set: 32 ms
 class Solution {
     public int partitionString(String s) {
         HashSet<Character> set = new HashSet<>();
@@ -11,5 +12,22 @@ class Solution {
             set.add(c);
         }
         return partitions+1;
+    }
+}
+
+// Counter array: 8 ms
+class Solution {
+    public int partitionString(String s) {
+        boolean[] freq = new boolean[26];
+        int partitions = 1;
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (freq[c-'a']) {
+                partitions++;
+                freq = new boolean[26];
+            }
+            freq[c-'a'] = true;
+        }
+        return partitions;
     }
 }
