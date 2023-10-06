96f4ed08ebf18652809e6b4e2b369f283b231103
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-10-06 12:59:36 +0530

    Update 343_IntegerBreak.java

--- 343_IntegerBreak.java
+++ 343_IntegerBreak.java
@@ -1,10 +1,11 @@
+// Dynamic programming: 1 ms
 class Solution {
     public int integerBreak(int n) {
         if (n <= 1) {
             return -1;
         }
         int[] dp = new int[n+1];
-        dp[1] =1; dp[2] = 1;
+        dp[1] = 1; dp[2] = 1;
         for (int i=3; i<=n; i++) {
             dp[i] = dp[2] * (i-2);
             for (int j=2; j<=i-1; j++) {
@@ -12,5 +13,20 @@ class Solution {
             }
         }
         return dp[n];
+    }
+}
+
+// My code (simple math): 0 ms
+class Solution {
+    public int integerBreak(int n) {
+        if (n < 4) {
+            return n-1;
+        }
+        int ans = 1;
+        while (n > 4) {
+            ans *= 3;
+            n -= 3;
+        }
+        return ans*n;
     }
 }
