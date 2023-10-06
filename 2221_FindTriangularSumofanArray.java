3bea7ef150e67742a98f6bdf96fdc22175ef0772
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-10-06 17:34:17 +0530

    Create 2221_FindTriangularSumofanArray.java

---
+++ 2221_FindTriangularSumofanArray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int triangularSum(int[] nums) {
+        int n = nums.length;
+        int[] cache = nums;
+        while (n-- > 1) {
+            for (int i=0; i<n; i++) {
+                cache[i] = (nums[i] + nums[i+1]) % 10;
+            }
+        }
+        return cache[0];
+    }
+}
