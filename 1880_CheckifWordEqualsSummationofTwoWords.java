3998a10faf1a55394de9d67eac48760c391d35be
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-10-12 00:18:15 +0530

    Update 1880_CheckifWordEqualsSummationofTwoWords.java

--- 1880_CheckifWordEqualsSummationofTwoWords.java
+++ 1880_CheckifWordEqualsSummationofTwoWords.java
@@ -11,3 +11,24 @@ class Solution {
         return get(firstWord) + get(secondWord) == get(targetWord);
     }
 }
+
+// Another approach
+class Solution {
+    public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+        int i = firstWord.length()-1, j = secondWord.length()-1, k = targetWord.length()-1, carry = 0;
+        while (i>-1 || j>-1 || k>-1) {
+            int first = i == -1 ? 0 : (int)firstWord.charAt(i--)-'a';
+            int second = j == -1 ? 0 : (int)secondWord.charAt(j--)-'a';
+            int third = k == -1 ? 0 : (int)targetWord.charAt(k--)-'a';
+            int sum = first + second;
+            if ((sum+carry)%10 != third) {
+                return false;
+            }
+            carry = sum+carry > 9 ? 1 : 0;
+        }
+        if (carry==1 && i==-1 && j==-1 && k==-1) {
+            return false;
+        }
+        return true;
+    }
+}
