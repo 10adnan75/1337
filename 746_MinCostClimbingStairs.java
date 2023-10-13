ccfe00b9e7344b2972a058f0f427f45a5b37c3dd
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-10-13 20:18:53 +0530

    Update 746_MinCostClimbingStairs.java

--- 746_MinCostClimbingStairs.java
+++ 746_MinCostClimbingStairs.java
@@ -6,3 +6,14 @@ class Solution {
         return Math.min(cost[cost.length-1], cost[cost.length-2]);
     }
 }
+
+// My code: dynamic programming
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length+2];
+        for (int i=cost.length-1; i>=0; i--) {
+            dp[i] = cost[i] + Math.min(dp[i+1], dp[i+2]);
+        }
+        return Math.min(dp[0], dp[1]);
+    }
+}
