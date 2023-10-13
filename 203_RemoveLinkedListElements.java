ebb0027c663f1d970d761be549edbaac7c9bd54a
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-10-14 01:57:19 +0530

    Update 203_RemoveLinkedListElements.java

--- 203_RemoveLinkedListElements.java
+++ 203_RemoveLinkedListElements.java
@@ -24,3 +24,32 @@ class Solution {
         return helper.next;
     }
 }
+
+// My code without using an extra space
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        ListNode temp = head, prev = head;
+        while (temp != null) {
+            if (temp.val == val) {
+                prev.next = temp.next;
+            } else {
+                prev = temp;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+}
