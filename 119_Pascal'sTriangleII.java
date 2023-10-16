be275d8839b7a53e2b183e69f676602d5786dc4a
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-10-16 19:56:01 +0530

    Update 119_Pascal'sTriangleII.java

--- 119_Pascal'sTriangleII.java
+++ 119_Pascal'sTriangleII.java
@@ -8,3 +8,27 @@ class Solution {
         return ans;
     }
 }
+
+// My code :)
+class Solution {
+    public List<Integer>[] ans = new ArrayList[34];
+
+    public List<Integer> generateRow(int row) {
+        List<Integer> l = new ArrayList<>();
+        for (int i=0; i<=row; i++) {
+            if (i==0 || i==row) {
+                l.add(1);
+            } else {
+                l.add(ans[row-1].get(i) + ans[row-1].get(i-1));
+            }
+        }
+        return l;
+    }
+
+    public List<Integer> getRow(int rowIndex) {
+        for (int i=0; i<34; i++) {
+            ans[i] = generateRow(i);
+        }
+        return ans[rowIndex];
+    }
+}
