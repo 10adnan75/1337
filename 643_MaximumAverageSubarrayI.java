23cfcd467e53099e9822c3cfe9f801677d6ae05b
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-10-18 11:50:27 +0530

    Update 643_MaximumAverageSubarrayI.java

--- 643_MaximumAverageSubarrayI.java
+++ 643_MaximumAverageSubarrayI.java
@@ -16,3 +16,19 @@ class Solution {
         return (double)maxSum/k;
     }
 }
+
+// Time efficient
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for (int i=0; i<k; i++) {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (int i=k; i<nums.length; i++) {
+            sum += nums[i] - nums[i-k];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return (double)maxSum/k;
+    }
+}
