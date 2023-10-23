4b821b98b4680a03869de8db65cf83746a4b0032
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-10-23 12:02:12 +0530

    Update 342_PowerofFour.java

--- 342_PowerofFour.java
+++ 342_PowerofFour.java
@@ -1,10 +1,28 @@
+// Iteration: 1 ms
 class Solution {
     public boolean isPowerOfFour(int n) {
         if (n < 1) {
             return false;
-        } while (n % 4 == 0) {
+        } 
+        while (n % 4 == 0) {
             n /= 4;
         }
         return n == 1;
+    }
+}
+
+// Recursion: 0 ms
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n < 1) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        if (n % 4 == 0) {
+            return isPowerOfFour(n/4);
+        }
+        return false;
     }
 }
