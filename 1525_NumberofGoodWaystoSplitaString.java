5f1d92cc4b3b2ea3b09b1ceaa46ce1cf4c45b983
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-10-26 00:44:58 +0530

    Create 1525_NumberofGoodWaystoSplitaString.java

---
+++ 1525_NumberofGoodWaystoSplitaString.java
@@ -0,0 +1,27 @@
+// TLE :(
+class Solution {
+    public int getCount(String s) {
+        Set<Character> set = new HashSet<>();
+        for (char c: s.toCharArray()) {
+            set.add(c);
+        }
+        return set.size();
+    }
+
+    public boolean checkSplits(String a, String b) {
+        return getCount(a) == getCount(b);
+    }
+
+    public int numSplits(String s) {
+        int n = s.length();
+        int[][] aux = new int[n][2];
+        int c = 0;
+        for (int i=1; i<n; i++) {
+            if (checkSplits(s.substring(0, i), s.substring(i, n))) {
+                c++;
+            }
+        }
+        return c;
+    }
+}
+
