9735b5935d7a29d7d689fc55d0747b3c7272a57a
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-10-26 01:02:15 +0530

    Update 1525_NumberofGoodWaystoSplitaString.java

--- 1525_NumberofGoodWaystoSplitaString.java
+++ 1525_NumberofGoodWaystoSplitaString.java
@@ -23,3 +23,28 @@ class Solution {
         return c;
     }
 }
+
+// Accepted :)
+class Solution {
+    public int numSplits(String s) {
+        int n = s.length(), c = 0;
+        int[][] aux = new int[n][2];
+        aux[0][0] = 1; aux[n-1][1] = 1;
+        Set<Character> set = new HashSet<>();
+        set.add(s.charAt(0));
+        for (int i=1; i<n; i++) {
+            set.add(s.charAt(i));
+            aux[i][0] = set.size();
+        }
+        set.clear();
+        set.add(s.charAt(n-1));
+        for (int i=n-2; i>=0; i--) {
+            set.add(s.charAt(i));
+            aux[i][1] = set.size();
+        }
+        for (int i=0; i<n-1; i++) {
+            c = aux[i][0] == aux[i+1][1] ? c+1 : c;
+        }
+        return c;
+    }
+}
