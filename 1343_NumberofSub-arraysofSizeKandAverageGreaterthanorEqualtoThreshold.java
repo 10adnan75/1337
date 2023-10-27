6eae47c0e3313c04b8039d662065d71fb6c2fdcb
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-10-27 16:24:35 +0530

    Update 1343_NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold.java

--- 1343_NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold.java
+++ 1343_NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold.java
@@ -1,3 +1,4 @@
+// 1499 ms, dumb me -_-
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
         int c = 0;
@@ -7,6 +8,27 @@ class Solution {
                 sum += arr[j];
             }
             if (sum / k >= threshold) {
+                c++;
+            }
+        }
+        return c;
+    }
+}
+
+// 3 ms, lol XD
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int c = 0;
+        int sum = 0;
+        for (int i=0; i<k; i++) {
+            sum += arr[i];
+        }
+        if (sum/k >= threshold) {
+            c++;
+        }
+        for (int i=k; i<arr.length; i++) {
+            sum += arr[i] - arr[i-k];
+            if (sum/k >= threshold) {
                 c++;
             }
         }
