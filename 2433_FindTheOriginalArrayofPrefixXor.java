908a23c000dcd102f5ee8fd1dbc885927a5fa441
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-10-31 14:22:18 +0530

    Update 2433_FindTheOriginalArrayofPrefixXor.java

--- 2433_FindTheOriginalArrayofPrefixXor.java
+++ 2433_FindTheOriginalArrayofPrefixXor.java
@@ -8,3 +8,13 @@ class Solution {
         return xor;
     }
 }
+
+// Space optimized
+class Solution {
+    public int[] findArray(int[] pref) {
+        for (int i=pref.length-1; i>0; i--) {
+            pref[i] = pref[i-1] ^ pref[i];
+        }
+        return pref;
+    }
+}
