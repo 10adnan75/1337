f7e60c6a90d0c53b34dbdc78717e9dac754c46fd
Adnan M Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-11-10 00:17:32 +0530

    Update 1128_NumberofEquivalentDominoPairs.java

--- 1128_NumberofEquivalentDominoPairs.java
+++ 1128_NumberofEquivalentDominoPairs.java
@@ -1,1 +1,26 @@
+class Solution {
+    public String hashCode(int[] n) {
+        return n[0] + " " + n[1];
+    }
 
+    public int computeCombination(int n) {
+        return n * (n-1) / 2;
+    }
+
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int c = 0;
+        Map<String, Integer> map = new HashMap<>();
+        for (int[] domino: dominoes) {
+            Arrays.sort(domino);
+            String s = hashCode(domino);
+            map.put(s, map.getOrDefault(s, 0)+1);
+        }
+        for (Map.Entry e: map.entrySet()) {
+            int val = (int)e.getValue();
+            if (val > 1) {
+                c += computeCombination(val);
+            }
+        }
+        return c;
+    }
+}
