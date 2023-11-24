fa861081bcc67cbf1872ba1a08c4bf6dc9ca9970
Adnan M Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-11-24 12:59:24 +0530

    Create 1561_MaximumNumberofCoinsYouCanGet.java

---
+++ 1561_MaximumNumberofCoinsYouCanGet.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int max = 0;
+        for (int i=piles.length/3; i<piles.length; i+=2) {
+            max += piles[i];
+        }
+        return max;
+    }
+}
