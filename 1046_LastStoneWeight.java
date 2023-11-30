7965ed28b8a692e5d4c0504f46b3b0320a16cbc7
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-11-30 17:20:19 +0530

    Update 1046_LastStoneWeight.java

--- 1046_LastStoneWeight.java
+++ 1046_LastStoneWeight.java
@@ -1,3 +1,4 @@
+// ArrayList: 2 ms
 class Solution {
     public int lastStoneWeight(int[] stones) {
         List<Integer> pq = new ArrayList<>();
@@ -11,5 +12,19 @@ class Solution {
             pq.add(diff);
         }
         return pq.get(0);
+    }
+}
+
+// PriorityQueue: 1 ms
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int stone: stones) {
+            pq.add(stone);
+        }
+        while (pq.size() > 1) {
+            pq.offer(pq.poll() - pq.poll());
+        }
+        return pq.poll();
     }
 }
