2656dda62b703a579adbf4bed40c4de9f913846a
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-12-01 11:21:44 +0530

    Update 1662_CheckIfTwoStringArraysareEquivalent.java

--- 1662_CheckIfTwoStringArraysareEquivalent.java
+++ 1662_CheckIfTwoStringArraysareEquivalent.java
@@ -1,3 +1,4 @@
+// Concatenation
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         StringBuilder sb1 = new StringBuilder();
@@ -9,5 +10,26 @@ class Solution {
             sb2.append(word);
         }
         return sb1.toString().equals(sb2.toString());
+    }
+}
+
+// Two Pointers
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        int w1 = 0, w2 = 0, left = 0, right = 0;
+        while (w1 < word1.length && w2 < word2.length) {
+            if (word1[w1].charAt(left++) != word2[w2].charAt(right++)) {
+                return false;
+            }
+            if (left == word1[w1].length()) {
+                w1++;
+                left = 0;
+            }
+            if (right == word2[w2].length()) {
+                w2++;
+                right = 0;
+            }
+        }
+        return w1 == word1.length && w2 == word2.length;
     }
 }
