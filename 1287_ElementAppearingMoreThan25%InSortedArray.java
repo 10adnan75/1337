d8df49ec09938ddb1b5fc56bcf16f723f1f35cb4
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-12-11 12:15:11 +0530

    Update 1287_ElementAppearingMoreThan25%InSortedArray.java

--- 1287_ElementAppearingMoreThan25%InSortedArray.java
+++ 1287_ElementAppearingMoreThan25%InSortedArray.java
@@ -1,3 +1,22 @@
+// 2 ms
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        int[] freq = new int[100001];
+        for (int i : arr) {
+            freq[i]++;
+        }
+        int ans = 0;
+        double n = arr.length * 0.25;
+        for (int i=0; i<freq.length; i++) {
+            if (freq[i] > n) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+// 3 ms
 class Solution {
     public int findSpecialInteger(int[] arr) {
         int[] freq = new int[100001];
