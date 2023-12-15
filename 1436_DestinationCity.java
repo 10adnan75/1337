6e2adcd28275ceed5bfa11c0543836efc439f803
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-12-15 15:39:23 +0530

    Update 1436_DestinationCity.java

--- 1436_DestinationCity.java
+++ 1436_DestinationCity.java
@@ -1,3 +1,4 @@
+// 9 ms
 class Solution {
     public String destCity(List<List<String>> paths) {
         int i = 0;
@@ -16,5 +17,21 @@ class Solution {
             }
         }
         return finalDestination;
+    }
+}
+
+// 2 ms
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> set = new HashSet<>();
+        for (List<String> path: paths) {
+            set.add(path.get(0));
+        }        
+        for (List<String> path: paths) {
+            if (!set.contains(path.get(1))) {
+                return path.get(1);
+            }
+        }
+        return "";
     }
 }
