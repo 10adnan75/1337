be50005e3d28e8bab2766004e1b94d5aa48c5cb1
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2023-12-20 19:17:52 +0530

    Update 2706_BuyTwoChocolates.java

--- 2706_BuyTwoChocolates.java
+++ 2706_BuyTwoChocolates.java
@@ -1,3 +1,20 @@
+// 1 ms
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int min = Math.min(prices[0], prices[1]), sMin = Math.max(prices[0], prices[1]);
+        for (int i=2; i<prices.length; i++){
+            if (prices[i] < min) {
+                sMin = min;
+                min = prices[i];
+            } else if (prices[i] < sMin) {
+                sMin = prices[i];
+            }
+        }
+        return (min + sMin <= money) ? money - min - sMin : money;
+    }
+}
+
+// 2 ms
 class Solution {
     public int buyChoco(int[] prices, int money) {
         Arrays.sort(prices);
