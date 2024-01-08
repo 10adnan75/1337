6e258ef8b8bc128de7d2ffd597743a6c289896a5
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-01-08 10:18:21 +0530

    Create 938_RangeSumofBST.java

---
+++ 938_RangeSumofBST.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        int rangeSum = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            int val = node.val;
+            if (val >= low && val <= high) {
+                rangeSum += val;
+            }
+            if (node.left != null) {
+                q.offer(node.left);
+            }
+            if (node.right != null) {
+                q.offer(node.right);
+            }
+        }
+        return rangeSum;
+    }
+}
