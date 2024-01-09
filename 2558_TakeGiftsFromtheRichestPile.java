6249a7360007a43161c49815499475dc2d80b5c1
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-01-09 14:17:47 +0530

    Update 2558_TakeGiftsFromtheRichestPile.java

--- 2558_TakeGiftsFromtheRichestPile.java
+++ 2558_TakeGiftsFromtheRichestPile.java
@@ -16,3 +16,21 @@ class Solution {
 }
 
 // Priority Queue
+// Runtime: 5 ms, Beats 88.84%
+// Memory: 42.38 MB, Beats 27.43%
+class Solution {
+    public long pickGifts(int[] gifts, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int gift: gifts) {
+            pq.offer(gift);
+        }
+        while (k-- > 0) {
+            pq.offer((int)Math.floor(Math.sqrt(pq.poll())));
+        }
+        long sum = 0;
+        for (int gift: pq) {
+            sum += gift;
+        }
+        return sum;
+    }
+}
