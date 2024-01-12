124997eb8f2638ef58f94bc409d627ce75f463ba
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-01-12 19:45:02 +0530

    Update 1704_DetermineifStringHalvesAreAlike.java

--- 1704_DetermineifStringHalvesAreAlike.java
+++ 1704_DetermineifStringHalvesAreAlike.java
@@ -18,3 +18,26 @@ class Solution {
         return countVowels(s.substring(0, s.length()/2)) == countVowels(s.substring(s.length()/2, s.length()));
     }
 }
+
+// A little optimization.
+class Solution {
+    public boolean isVowel(char c) {
+        c = Character.toLowerCase(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    public int getCountVowel(String s) {
+        int c = 0;
+        for (int i=0; i<s.length(); i++) {
+            if (isVowel(s.charAt(i))) {
+                c++;
+            }
+        }
+        return c;
+    }
+
+    public boolean halvesAreAlike(String s) {
+        int n = s.length();
+        return getCountVowel(s.substring(0, n/2)) == getCountVowel(s.substring(n/2, n));
+    }
+}
