18832aa5ff26ad6e498a251fe6f8f482f74676f8
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-01-13 19:56:39 +0530

    Update 1347_MinimumNumberofStepstoMakeTwoStringsAnagram.java

--- 1347_MinimumNumberofStepstoMakeTwoStringsAnagram.java
+++ 1347_MinimumNumberofStepstoMakeTwoStringsAnagram.java
@@ -1,3 +1,4 @@
+// 8 ms
 class Solution {
     public int minSteps(String s, String t) {
         int[] freq = new int[26];
@@ -14,5 +15,25 @@ class Solution {
             }
         }
         return min;
+    }
+}
+
+// 6 ms
+class Solution {
+    public int minSteps(String s, String t) {
+        int[] freq = new int[26];
+        for (char c: t.toCharArray()) {
+            freq[c-'a']++;
+        }
+        for (char c: s.toCharArray()) {
+            freq[c-'a']--;
+        }
+        int c = 0;
+        for (int i: freq) {
+            if (i < 0) {
+                c += Math.abs(i);
+            }
+        }
+        return c;
     }
 }
