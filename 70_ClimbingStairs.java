b3bcabaf8e2f737657e7930859f581fe4e21ab29
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-01-18 16:27:16 +0530

    Update 70_ClimbingStairs.java

--- 70_ClimbingStairs.java
+++ 70_ClimbingStairs.java
@@ -26,3 +26,16 @@ class Solution {
         return cache[n];
     }
 }
+
+// My code
+class Solution {
+    public int climbStairs(int n) {
+        int[] steps = new int[n+1];
+        steps[0] = 1;
+        steps[1] = 2;
+        for (int i=2; i<=n; i++) {
+            steps[i] = steps[i-1] + steps[i-2];
+        }
+        return steps[n-1];
+    }
+}
