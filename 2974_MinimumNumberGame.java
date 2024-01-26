2bb83b9d408428325efa564b6317ac024181b641
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-01-26 16:16:21 +0530

    Update 2974_MinimumNumberGame.java

--- 2974_MinimumNumberGame.java
+++ 2974_MinimumNumberGame.java
@@ -1,3 +1,22 @@
+// 3 ms
+class Solution {
+    public int[] numberGame(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int n: nums) {
+            pq.offer(n);
+        }
+        int[] ans = new int[nums.length];
+        int i = 0;
+        while (!pq.isEmpty()) {
+            int alice = pq.poll(), bob = pq.poll();
+            ans[i++] = bob;
+            ans[i++] = alice;
+        }
+        return ans;
+    }
+}
+
+// 4 ms
 class Solution {
     public int[] numberGame(int[] nums) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
