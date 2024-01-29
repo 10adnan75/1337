97dc98dadb1dc19933b384d59a033b48cd39d85a
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-01-29 11:16:28 +0530

    Update 232_ImplementQueueUsingStacks.java

--- 232_ImplementQueueUsingStacks.java
+++ 232_ImplementQueueUsingStacks.java
@@ -1,3 +1,50 @@
+// Amortized
+class MyQueue {
+    private int top;
+    public Stack<Integer> s1, s2;
+
+    public MyQueue() {
+        s1 = new Stack<>(); 
+        s2 = new Stack<>();
+    }
+    
+    public void push(int x) {
+        if (s1.isEmpty()) {
+            top = x;
+        }
+        s1.push(x);
+    }
+    
+    public int pop() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+    
+    public int peek() {
+        if (!s2.isEmpty()) {
+            return s2.peek();
+        }
+        return top;
+    }
+    
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+
 class MyQueue {
 
     Stack<Object> s1;
