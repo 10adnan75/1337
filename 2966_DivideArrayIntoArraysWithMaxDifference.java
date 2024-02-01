87f1be85bd4ee984c9a63c121739c52445c40d43
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-02-01 12:42:09 +0530

    Update 2966_DivideArrayIntoArraysWithMaxDifference.java

--- 2966_DivideArrayIntoArraysWithMaxDifference.java
+++ 2966_DivideArrayIntoArraysWithMaxDifference.java
@@ -1,3 +1,21 @@
+// Optimized: 24 ms
+class Solution {
+    public int[][] divideArray(int[] nums, int k) {
+        int[][] ans = new int[nums.length/3][3];
+        Arrays.sort(nums);
+        for (int i=0, j=0; i<nums.length; i+=3, j++) {
+            if (nums[i+2]-nums[i] > k) {
+                return new int[][] {};
+            }
+            ans[j][0] = nums[i];
+            ans[j][1] = nums[i+1];
+            ans[j][2] = nums[i+2];
+        }
+        return ans;
+    }
+}
+
+// 126 ms
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
         int[][] ans = new int[nums.length/3][3];
