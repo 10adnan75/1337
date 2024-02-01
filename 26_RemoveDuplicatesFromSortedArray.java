4351efa6ac3f4d7405ccf335998f84aeb6b870ed
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-02-01 22:39:46 +0530

    Update 26_RemoveDuplicatesFromSortedArray.java

--- 26_RemoveDuplicatesFromSortedArray.java
+++ 26_RemoveDuplicatesFromSortedArray.java
@@ -8,3 +8,20 @@ class Solution {
         return currentIndex+1;
     }
 }
+
+// my code
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int[] freq = new int[201];
+        for (int num: nums) {
+            freq[num+100]++;
+        }
+        int j = 0;
+        for (int i=0; i<freq.length; i++) {
+            if (freq[i] != 0) {
+                nums[j++] = i-100;
+            }
+        }
+        return j;
+    }
+}
