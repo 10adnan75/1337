41f0b6a955d74b261b5086182d532f50a0e4daa1
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-02-06 14:55:02 +0530

    Update 49_GroupAnagrams.java

--- 49_GroupAnagrams.java
+++ 49_GroupAnagrams.java
@@ -52,3 +52,22 @@ class Solution {
         return ans;
     }
 }
+
+// Optimized
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i=0; i<strs.length; i++) {
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String s = new String(chars);
+            List<String> l = new ArrayList<>();
+            if (map.containsKey(s)) {
+                l = map.get(s);
+            } 
+            l.add(strs[i]);
+            map.put(s, l);
+        } 
+        return new ArrayList<List<String>>(map.values());
+    }
+}
