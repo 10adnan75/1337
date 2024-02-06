f321a57e6b83028139b7d3b02690ab278f922572
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-02-06 14:51:53 +0530

    Update 49_GroupAnagrams.java

--- 49_GroupAnagrams.java
+++ 49_GroupAnagrams.java
@@ -20,3 +20,35 @@ class Solution {
         return Arrays.toString(map);
     }
 }
+
+// My code
+class Solution {
+    public String[] sortStrs(String[] strs) {
+        String[] ans = new String[strs.length];
+        for (int i=0; i<ans.length; i++) {
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            ans[i] = new String(chars);
+        }
+        return ans;
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        String[] copyStrs = sortStrs(strs);
+        List<List<String>> ans = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i=0; i<strs.length; i++) {
+            String s = copyStrs[i];
+            List<String> l = new ArrayList<>();
+            if (map.containsKey(s)) {
+                l = map.get(s);
+            } 
+            l.add(strs[i]);
+            map.put(s, l);
+        } 
+        for (Map.Entry e: map.entrySet()) {
+            ans.add((List<String>)e.getValue());
+        }
+        return ans;
+    }
+}
