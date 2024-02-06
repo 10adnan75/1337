77de7640ea2eb4242d664db2648fb3f94005c734
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-02-06 22:42:48 +0530

    Update 11_ContainerWithMostWater.java

--- 11_ContainerWithMostWater.java
+++ 11_ContainerWithMostWater.java
@@ -15,3 +15,18 @@ class Solution {
         return ans;
     }
 }
+
+// My code: TLE
+class Solution {
+    public int maxArea(int[] height) {
+        int max = 0;
+        for (int i=0; i<height.length; i++) {
+            for (int j=i+1; j<height.length; j++) {
+                max = Math.max(max, Math.min(height[i], height[j])*(j-i));
+            }
+        }
+        return max;
+    }
+}
+
+// My code: Optimized
