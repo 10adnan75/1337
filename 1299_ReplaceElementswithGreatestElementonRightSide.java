40075f40def9ecb544ef4fe39be11b19ecd42487
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-02-09 11:53:36 +0530

    Update 1299_ReplaceElementswithGreatestElementonRightSide.java

--- 1299_ReplaceElementswithGreatestElementonRightSide.java
+++ 1299_ReplaceElementswithGreatestElementonRightSide.java
@@ -11,3 +11,17 @@ class Solution {
         return ans;
     }
 }
+
+// In-Place
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int max = arr[arr.length-1];
+        for (int i=arr.length-2; i>=0; i--) {
+            int temp = arr[i];
+            arr[i] = max;
+            max = Math.max(max, temp);
+        }
+        arr[arr.length-1] = -1;
+        return arr;
+    }
+}
