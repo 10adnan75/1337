0e24c368d87253fe19d4ddd1472d5819694dd86a
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-02-12 14:11:16 +0530

    Update 169_MajorityElement.java

--- 169_MajorityElement.java
+++ 169_MajorityElement.java
@@ -22,3 +22,19 @@ class Solution {
         return candidate;
     }
 }
+
+// My code
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n: nums) {
+            map.put(n, map.getOrDefault(n, 0)+1);
+        }
+        for (Map.Entry e: map.entrySet()) {
+            if ((int) e.getValue() > nums.length/2) {
+                return (int) e.getKey();
+            }
+        }
+        return -1;
+    }
+}
