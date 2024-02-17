3aa606270a60b19a5dc8a1f99ac7d84fcc36895d
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-02-17 23:43:36 +0530

    Update 1642_FurthestBuildingYouCanReach.java

--- 1642_FurthestBuildingYouCanReach.java
+++ 1642_FurthestBuildingYouCanReach.java
@@ -1,3 +1,99 @@
+// 11/78 test cases passed
+class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        System.out.println(laddersPQ(heights, bricks, ladders));
+        System.out.println(bricksPQ(heights, bricks, ladders));
+        return Math.max(laddersPQ(heights, bricks, ladders), bricksPQ(heights, bricks, ladders));
+    }
+
+    public int laddersPQ(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int n = heights.length;
+        for (int i=1; i<n; i++) {
+            int height = heights[i] - heights[i-1];
+            if (height > 0) {
+                pq.offer(height);
+            }
+        }
+        int building = 0;
+        for (int i=1; i<n; i++) {
+            if (pq.isEmpty()) {
+                return n-1;
+            }
+            int height = heights[i] - heights[i-1];
+            if (height > 0) {
+                if (pq.peek() == height) {
+                    if (bricks - height >= 0) {
+                        bricks = bricks - height;
+                    } else if (ladders > 0) {
+                        ladders--;
+                    } else {
+                        break;
+                    }
+                    pq.poll();
+                } else if (pq.peek() > height) {
+                    if (ladders > 0) {
+                        ladders--;
+                    } else if (bricks - height >= 0) {
+                        bricks = bricks - height;
+                    } else {
+                        break;
+                    }
+                    pq.remove(height);
+                } else {
+                    break;
+                }
+            } 
+            building++;
+        }
+        return building;
+    } 
+
+    public int bricksPQ(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int n = heights.length;
+        for (int i=1; i<n; i++) {
+            int height = heights[i] - heights[i-1];
+            if (height > 0) {
+                pq.offer(height);
+            }
+        }
+        int building = 0;
+        for (int i=1; i<n; i++) {
+            if (pq.isEmpty()) {
+                return n-1;
+            }
+            int height = heights[i] - heights[i-1];
+            if (height > 0) {
+                if (pq.peek() == height) {
+                    if (ladders > 0) {
+                        ladders--;
+                    } else if (bricks - height >= 0) {
+                        bricks = bricks - height;
+                    } else {
+                        break;
+                    }
+                    pq.poll();
+                } else if (pq.peek() > height) {
+                    if (bricks - height >= 0) {
+                        bricks = bricks - height;
+                    } else if (ladders > 0) {
+                        ladders--;
+                    } else {
+                        break;
+                    }
+                    pq.remove(height);
+                } else {
+                    break;
+                }
+            } 
+            building++;
+        }
+        return building;
+    }
+}
+
+// 71/78 test cases passed
 class Solution {
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
@@ -42,3 +138,5 @@ class Solution {
         return building;
     }
 }
+
+// All test cases passed
