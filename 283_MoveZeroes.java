eecdf35ffd4e8bd2e3a8eef45ae08e2ec84f93bc
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-02-21 12:03:12 +0530

    Update 283_MoveZeroes.java

--- 283_MoveZeroes.java
+++ 283_MoveZeroes.java
@@ -13,7 +13,7 @@ class Solution {
     }
 }
 
-// My code
+// My code: auxiliary-space
 class Solution {
     public void moveZeroes(int[] nums) {
         int[] ans = new int[nums.length];
@@ -24,6 +24,21 @@ class Solution {
         }
         for (int i=0; i<nums.length; i++) {
             nums[i] = ans[i];
+        }
+    }
+}
+
+// My code: in-place
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonZero = 0;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZero++]= nums[i];
+            }
+        }
+        for (int i=nonZero; i<nums.length; i++) {
+            nums[i] = 0;
         }
     }
 }
