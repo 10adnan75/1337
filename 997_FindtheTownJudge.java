2b1e72f23d29a12894126848523012417bda934f
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-02-22 12:11:33 +0530

    Update 997_FindtheTownJudge.java

--- 997_FindtheTownJudge.java
+++ 997_FindtheTownJudge.java
@@ -14,3 +14,20 @@ class Solution {
         return judge;
     }
 }
+
+// My code
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] aux = new int[n+1];
+        for (int[] t: trust) {
+            aux[t[0]]--;
+            aux[t[1]]++;
+        }
+        for (int i=1; i<aux.length; i++) {
+            if (aux[i] == n-1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
