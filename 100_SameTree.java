2c7d435b2e77ce5fc20be03fb3980395bfd92584
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-02-26 13:00:36 +0530

    Update 100_SameTree.java

--- 100_SameTree.java
+++ 100_SameTree.java
@@ -24,3 +24,54 @@ class Solution {
         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
+
+// My code
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return getTree(p).equals(getTree(q));       
+    }
+
+    public String getTree(TreeNode t) {
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(t);
+        while (!q.isEmpty()) {
+            boolean f = true;
+            TreeNode n = q.poll();
+            sb.append(n.val);
+            if (n.left != null) {
+                q.offer(n.left);
+            }
+            if (n.left == null && n.right != null) {
+                f = !f;
+                sb.append(" ");
+                q.offer(n.right);
+            }
+            if (n.right != null && f) {
+                q.offer(n.right);
+            }
+        }
+        return sb.toString();
+    }
+}
