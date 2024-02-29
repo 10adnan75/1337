33f7d2a14857f81b524d0b4632146e91fb88b5d3
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-02-29 19:46:44 +0530

    Update 1446_ConsecutiveCharacters.java

--- 1446_ConsecutiveCharacters.java
+++ 1446_ConsecutiveCharacters.java
@@ -1,16 +1,5 @@
 class Solution {
     public int maxPower(String s) {
-        // int i = 0, c = 1;
-        // while (i < s.length()) {
-        //     int j = i+1, max = 1;
-        //     while (j < s.length() && s.charAt(i) == s.charAt(j)) {
-        //         max++;
-        //         j++;
-        //     }
-        //     c = Math.max(max, c);
-        //     i = j;
-        // }
-        // return c;
         int max = 1, ans = 1;
         for (int i=0; i<s.length()-1; i++) {
             if (s.charAt(i) == s.charAt(i+1)) {
