43c4a320d8fcea23a5eb1fe84b7354666459c20f
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-03-21 22:07:11 +0530

    Update 206_ReverseLinkedList.java

--- 206_ReverseLinkedList.java
+++ 206_ReverseLinkedList.java
@@ -36,3 +36,34 @@ class Solution {
         return rest;
     }
 }
+
+// my code
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        List<Integer> l = new ArrayList<>();
+        while (head != null) {
+            l.add(head.val);
+            head = head.next;
+        }
+        int i = 0;
+        head = new ListNode(l.get(i++));
+        while (i < l.size()) {
+            ListNode newNode = new ListNode(l.get(i++), head);
+            head = newNode;
+        }
+        return head;
+    }
+}
