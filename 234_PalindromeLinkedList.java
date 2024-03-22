05c972d2ca8706b7a8edcbd3755a1081a6674631
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-03-22 13:42:01 +0530

    Update 234_PalindromeLinkedList.java

--- 234_PalindromeLinkedList.java
+++ 234_PalindromeLinkedList.java
@@ -1,3 +1,4 @@
+// Nov 2023
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -26,5 +27,36 @@ class Solution {
             head = head.next;
         }
         return help(sb.toString());
+    }
+}
+
+// Mar 2024
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+        while (head != null) {
+            sb.append(head.val);
+            head = head.next;
+        }
+        return check(sb.toString(), sb.length());
+    }
+
+    public boolean check(String s, int n) {
+        for (int i=0; i<n/2; i++) {
+            if (s.charAt(i) != s.charAt(n-1-i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
