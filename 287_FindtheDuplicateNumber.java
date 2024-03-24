2b59842e29f5f727e62334b15cecaa171889c52d
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-03-25 00:34:44 +0530

    Update 287_FindtheDuplicateNumber.java

--- 287_FindtheDuplicateNumber.java
+++ 287_FindtheDuplicateNumber.java
@@ -27,3 +27,20 @@ class Solution {
         return -1;
     }
 }
+
+// Floyd's tortoise and hare algorithm
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0], fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
