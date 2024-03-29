64f5db4bc6b46e1960255a1db1964d604d05ed0d
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-03-29 18:57:22 +0530

    Create 2962_CountSubarraysWhereMaxElementAppearsatLeastKTimes.java

---
+++ 2962_CountSubarraysWhereMaxElementAppearsatLeastKTimes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long countSubarrays(int[] nums, int k) {
+        long c = 0;
+        int max = 0;
+        for (int num: nums) {
+            max = Math.max(max, num);
+        }
+        for (int i=0; i<nums.length; i++) {
+            int j = i, maxC = 0;
+            while (j < nums.length) {
+                if (nums[j] == max) {
+                    maxC++;
+                }
+                if (maxC >= k) {
+                    c++;
+                }
+                j++;
+            }
+        }
+        return c;
+    }
+}
