70ff10851e65f2ee4d711fb6030239f621b26e58
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-04-01 14:37:05 +0530

    Update 58_LengthofLastWord.java

--- 58_LengthofLastWord.java
+++ 58_LengthofLastWord.java
@@ -1,6 +1,28 @@
+// 1 ms
 class Solution {
     public int lengthOfLastWord(String s) {
         String[] strArr = s.split(" ");
         return strArr[strArr.length-1].length();
+    }
+}
+
+// 0 ms
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int c = 0;
+        boolean flag = false;
+        for (int i=s.length()-1; i>=0; i--) {
+            if (s.charAt(i) == ' ') {
+                if (flag) {
+                    break;
+                }
+                continue;
+            }
+            if (s.charAt(i) != ' ') {
+                flag = true;
+                c++;
+            }
+        }
+        return c;
     }
 }
