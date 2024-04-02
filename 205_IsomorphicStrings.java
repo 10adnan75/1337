0f4efada7eebdf567ccd09767b1fd4398b891103
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-04-03 01:40:18 +0530

    Update 205_IsomorphicStrings.java

--- 205_IsomorphicStrings.java
+++ 205_IsomorphicStrings.java
@@ -23,3 +23,54 @@ class Solution {
         return true;
     }
 }
+
+// redundancy elimination
+
+
+// 43/45 test cases passed! (need to work on this)
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        return helper(s, t) && getOrder(s).equals(getOrder(t));
+    }
+
+    public String getOrder(String str) {
+        String o = "";
+        int i = 0, c = 1;
+        while (i < str.length()) {
+            char ch = str.charAt(i);
+            int j = i + 1;
+            while (j < str.length() && str.charAt(j) == ch) {
+                j++;
+                c++;
+            }
+            o += c;
+            c = 1;
+            i = j;
+        }
+        return o;
+    }
+
+    public boolean helper(String s, String t) {
+        int[] f1 = new int[256], f2 = new int[256];
+        int c = 0, v = 0;
+        for (char ch: s.toCharArray()) {
+            f1[ch]++;
+        }
+        for (char ch: t.toCharArray()) {
+            f2[ch]++;
+        }
+        for (int i: f1) {
+            if (i != 0) {
+                c++;
+                v += i;
+            }
+        }
+        for (int i: f2) {
+            if (i != 0) {
+                c--;
+                v -= i;
+            }
+        }
+        return c != 0 ? false : v != 0 ? false : true; 
+    }
+}
