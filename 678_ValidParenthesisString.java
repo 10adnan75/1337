5e86ac0c62f78d729084e6a18b2396519b28fad0
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-04-08 02:19:27 +0530

    Update 678_ValidParenthesisString.java

--- 678_ValidParenthesisString.java
+++ 678_ValidParenthesisString.java
@@ -22,3 +22,33 @@ class Solution {
         return min == 0;
     }
 }
+
+// my code: passed 70/83 testcases (need to work on this)
+class Solution {
+    public boolean checkValidString(String s) {
+        int c = 0, k = 0;
+        Stack<Character> stack = new Stack<>();
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                if (c > 0 && k == 0) {
+                    c = 0;
+                }
+                stack.push('(');
+            } else if (s.charAt(i) == ')') {
+                k++;
+                if (stack.empty()) {
+                    if (c == 0) {
+                        return false;
+                    } else {
+                        c--;
+                        continue;
+                    }
+                }
+                stack.pop();
+            } else {
+                c++;
+            }
+        }
+        return stack.empty() ? true : c - stack.size() >= 0;
+    }
+}
