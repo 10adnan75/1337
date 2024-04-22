3ce89400c20d2be033bc647cd3cef31c42a9ce2b
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-04-23 03:26:50 +0530

    Update 3024_TypeofTriangle.java

--- 3024_TypeofTriangle.java
+++ 3024_TypeofTriangle.java
@@ -1,8 +1,24 @@
-
+// Sort
 class Solution {
     public String triangleType(int[] nums) {
         Arrays.sort(nums);
         if (nums[0] + nums[1] > nums[2]) {
+            if (nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2]) {
+                return "scalene"; 
+            } else if (nums[0] == nums[1] && nums[1] == nums[2]) {
+                return "equilateral";
+            } else {
+                return "isosceles";
+            }
+        }
+        return "none";
+    }
+}
+
+// No sort
+class Solution {
+    public String triangleType(int[] nums) {
+        if (nums[0]+nums[1] > nums[2] && nums[1]+nums[2] > nums[0] && nums[0]+nums[2] > nums[1]) {
             if (nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2]) {
                 return "scalene"; 
             } else if (nums[0] == nums[1] && nums[1] == nums[2]) {
