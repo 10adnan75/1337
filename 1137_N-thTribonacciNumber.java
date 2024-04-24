5f26f962ffffea5ae135e501be3496acbe9e8909
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-04-25 02:50:48 +0530

    Update 1137_N-thTribonacciNumber.java

--- 1137_N-thTribonacciNumber.java
+++ 1137_N-thTribonacciNumber.java
@@ -13,3 +13,21 @@ class Solution {
         return series[n];
     }
 }
+
+//
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return n;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int[] arr = new int[n+1];
+        arr[1] = 1;
+        arr[2] = 1;
+        for (int i=3; i<=n; i++) {
+            arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+        }
+        return arr[n];
+    }
