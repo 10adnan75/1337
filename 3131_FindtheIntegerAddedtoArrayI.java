32f49af4d643f60ff8825bffbb16172151c0bfc5
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-04-30 10:50:16 +0530

    Update 3131_FindtheIntegerAddedtoArrayI.java

--- 3131_FindtheIntegerAddedtoArrayI.java
+++ 3131_FindtheIntegerAddedtoArrayI.java
@@ -1,7 +1,23 @@
+// 2 ms
 class Solution {
     public int addedInteger(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         return nums2[0] - nums1[0]; 
+    }
+}
+
+// 0 ms
+class Solution {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        return getMin(nums2) - getMin(nums1); 
+    }
+
+    public int getMin(int[] arr) {
+        int min = arr[0];
+        for (int i=1; i<arr.length; i++) {
+            min = Math.min(min, arr[i]);
+        }
+        return min;
     }
 }
