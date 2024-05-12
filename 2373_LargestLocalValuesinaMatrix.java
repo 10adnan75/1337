054a32242fa21183f355271d4940c187270fa45f
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-05-12 19:49:33 +0530

    Update 2373_LargestLocalValuesinaMatrix.java

--- 2373_LargestLocalValuesinaMatrix.java
+++ 2373_LargestLocalValuesinaMatrix.java
@@ -20,3 +20,27 @@ class Solution {
         return ans;
     }
 }
+
+// 
+class Solution {
+    public int[][] largestLocal(int[][] grid) {
+        int n = grid.length;
+        int[][] ans = new int[n-2][n-2];
+        for (int i=0; i<n-2; i++) {
+            for (int j=0; j<n-2; j++) {
+                ans[i][j] = getMax(grid, i, j);
+            }
+        }
+        return ans;
+    }
+
+    public int getMax(int[][] grid, int r, int c) {
+        int max = 0;
+        for (int i=r; i<r+3; i++) {
+            for (int j=c; j<c+3; j++) {
+                max = Math.max(max, grid[i][j]);
+            }
+        }
+        return max;
+    }
+}
