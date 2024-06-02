03884ac9cae26d48df8c845c3e44eed18b78b751
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-06-02 20:49:10 +0530

    Update 344_ReverseString.java

--- 344_ReverseString.java
+++ 344_ReverseString.java
@@ -8,3 +8,15 @@ class Solution {
         }
     }
 }
+
+// My code
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        for (int i=0; i<n/2; i++) {
+            char c = s[i];
+            s[i] = s[n-i-1];
+            s[n-i-1] = c;
+        }
+    }
+}
