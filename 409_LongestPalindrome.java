2f68d3112f7bfb55a80b3fb71c925a8cd406b6fa
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-06-04 20:42:09 +0530

    Update 409_LongestPalindrome.java

--- 409_LongestPalindrome.java
+++ 409_LongestPalindrome.java
@@ -17,3 +17,33 @@ class Solution {
         return count;
     }
 }
+
+// My code: 2 ms :)
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] freqL = new int[26], freqU = new int[26];
+        int c = 0;
+        for (char ch: s.toCharArray()) {
+            if (Character.isLowerCase(ch)) {
+                freqL[ch-'a']++;
+            } else {
+                freqU[ch-'A']++;
+            }
+        }
+        for (int i=0; i<26; i++) {
+            int l = freqL[i], u = freqU[i];
+            if (l > 0) {
+                c += (l&1) == 1 ? l-1 : l;
+            }
+            if (u > 0) {
+                c += (u&1) == 1 ? u-1 : u;
+            }
+        }
+        for (int i=0; i<26; i++) {
+            if ((freqL[i]&1) == 1 || (freqU[i]&1) == 1) {
+                return c+1;
+            }
+        }
+        return c;
+    }
+}
