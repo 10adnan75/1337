4e9b93650a05ef4be94ae3aa32fb6de1eafb5ace
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-06-06 20:11:19 +0530

    Update 1002_FindCommonCharacters.java

--- 1002_FindCommonCharacters.java
+++ 1002_FindCommonCharacters.java
@@ -1,3 +1,4 @@
+// 4 ms
 class Solution {
     public List<String> commonChars(String[] words) {
         int[][] map = new int[words.length][26];
@@ -15,6 +16,36 @@ class Solution {
             }
             while (min-- > 0) {
                 ans.add(c+"");
+            }
+        }
+        return ans;
+    }
+}
+
+// 5 ms
+class Solution {
+    public List<String> commonChars(String[] words) {
+        List<String> ans = new ArrayList<>();
+        int[][] bool = new int[words.length][26];
+        for (int i=0; i<words.length; i++) {
+            for (int j=0; j<words[i].length(); j++) {
+                bool[i][words[i].charAt(j)-'a']++;
+            }
+        }
+        for (int i=0; i<26; i++) {
+            boolean flag = false;
+            int min = Integer.MAX_VALUE;
+            for (int j=0; j<words.length; j++) {
+                if (bool[j][i] == 0) {
+                    flag = true;
+                    break;
+                }
+                min = Math.min(min, bool[j][i]);
+            }
+            if (!flag) {
+                while (min-- > 0) {
+                    ans.add((char)(i+'a')+"");
+                }
             }
         }
         return ans;
