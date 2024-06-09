1eebb484ac34c818175a6fa485ed54a03b65e728
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-06-09 23:43:19 +0530

    Create 974_SubarraySumsDivisiblebyK.java

---
+++ 974_SubarraySumsDivisiblebyK.java
@@ -0,0 +1,16 @@
+// TLE :(
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int c = 0;
+        for (int i=0; i<nums.length; i++) {
+            int sum = 0;
+            for (int j=i; j<nums.length; j++) {
+                sum += nums[j];
+                if (sum % k == 0) {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}
