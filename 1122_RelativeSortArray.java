ae2e9b1ac7b441a1008239f1ada98c69b022a028
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-07-03 16:48:58 +0530

    Update 1122_RelativeSortArray.java

--- 1122_RelativeSortArray.java
+++ 1122_RelativeSortArray.java
@@ -18,3 +18,26 @@ class Solution {
         return arr1;
     }
 }
+
+// 
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] freq = new int[1001];
+        for (int a: arr1) {
+            freq[a]++;
+        }
+        int[] ans = new int[arr1.length];
+        int id = 0;
+        for (int a: arr2) {
+            while (freq[a]-- > 0) {
+                ans[id++] = a;
+            }
+        }
+        for (int i=0; i<freq.length; i++) {
+            while (freq[i]-- > 0) {
+                ans[id++] = i;
+            }
+        }
+        return ans;
+    }
+}
