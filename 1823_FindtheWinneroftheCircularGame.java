86fe952fa9a391a05fb3cd54b0300ec29f583c39
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-07-08 18:51:50 +0530

    Update 1823_FindtheWinneroftheCircularGame.java

--- 1823_FindtheWinneroftheCircularGame.java
+++ 1823_FindtheWinneroftheCircularGame.java
@@ -11,3 +11,45 @@ class Solution {
         return (helper(n-1, k) + k) % n;
     }
 }
+
+// My code
+class ListNode {
+    public int data;
+    public ListNode next;
+
+    public ListNode() {}
+
+    public ListNode(int n) {
+        this.data = n;
+    } 
+
+    public ListNode(int n, ListNode next) {
+        this.data = n;
+        this.next = next;
+    } 
+}
+
+class Solution {
+    public int findTheWinner(int n, int k) {
+        ListNode head = createList(n);
+        ListNode temp = head;
+        while (n-- > 1) {
+            
+        }
+        return 0;
+    }
+
+    public ListNode createList(int n) {
+        ListNode head = new ListNode();
+        int i = 1;
+        ListNode temp = head;
+        while (i < n) {
+            temp.data = i++;
+            ListNode newNode = new ListNode();
+            temp.next = newNode;
+            temp = newNode;
+        }
+        temp.data = n;
+        return head;
+    }
+}
