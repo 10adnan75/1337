e433f31b3e9a087bc6a2429caa37ea1fb80912ad
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-07-09 18:12:53 +0530

    Update 1701_AverageWaitingTime.java

--- 1701_AverageWaitingTime.java
+++ 1701_AverageWaitingTime.java
@@ -13,3 +13,16 @@ class Solution {
         return avg / customers.length;
     }
 }
+
+// Optimized
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+        double avg = 0.0;
+        int time = customers[0][0];
+        for (int[] customer: customers) {
+            time = customer[0] > time ? customer[0] + customer[1] : time + customer[1];
+            avg += (double)(time - customer[0]);
+        }
+        return avg / customers.length;
+    }
+}
