977c6215c337a92d40d1839202f8aea221f236b4
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-07-10 13:28:18 +0530

    Update 1598_CrawlerLogFolder.java

--- 1598_CrawlerLogFolder.java
+++ 1598_CrawlerLogFolder.java
@@ -14,3 +14,22 @@ class Solution {
         return c;
     }
 }
+
+// Follow-up (Stack)
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> path = new Stack<>();
+        for (String log: logs) {
+            char c = log.charAt(1);
+            if (c == '.') {
+                if (!path.empty()) {
+                    path.pop();
+                }
+            } else if (c != '/') {
+                path.push(log);
+            }
+        }
+        // System.out.print(path); // print path
+        return path.size();
+    }
+}
