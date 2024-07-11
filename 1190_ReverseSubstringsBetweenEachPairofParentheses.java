87a4b76943a8e0adaad744154d9b44dd66ea8572
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-07-12 02:34:32 +0530

    Update 1190_ReverseSubstringsBetweenEachPairofParentheses.java

--- 1190_ReverseSubstringsBetweenEachPairofParentheses.java
+++ 1190_ReverseSubstringsBetweenEachPairofParentheses.java
@@ -55,3 +55,29 @@ class Solution {
         return s;
     }
 }
+
+// Accepted
+class Solution {
+    public String reverseParentheses(String s) {
+        Stack<Integer> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for (char c: s.toCharArray()) {
+            if (c == '(') {
+                st.push(sb.length());
+            } else if (c == ')') {
+                reverse(sb, st.pop(), sb.length()-1);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public void reverse(StringBuilder sb, int start, int end) {
+        while (start < end) {
+            char temp = sb.charAt(start);
+            sb.setCharAt(start++, sb.charAt(end));
+            sb.setCharAt(end--, temp);
+        }
+    }
+}
