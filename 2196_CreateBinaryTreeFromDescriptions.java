81534ad021f02a5d7aef2aad7d74cb37684a8747
Adnan Mazharuddin Shaikh <52044177+10adnan75@users.noreply.github.com>
2024-07-15 19:37:29 +0530

    Update 2196_CreateBinaryTreeFromDescriptions.java

--- 2196_CreateBinaryTreeFromDescriptions.java
+++ 2196_CreateBinaryTreeFromDescriptions.java
@@ -56,3 +56,72 @@ class Solution {
         return -1;
     }
 }
+
+// Note: if TLE using []'s, use HashMaps :)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private Map<Integer, int[]> map = new HashMap<>();
+
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        int rootVal = 0;
+        createMap(descriptions);
+        Set<Integer> parents = new HashSet<>();
+        for (int[] desc: descriptions) {
+            parents.add(desc[1]);
+        }
+        for (int[] desc: descriptions) {
+            if (!parents.contains(desc[0])) {
+                rootVal = desc[0];
+                break;
+            }
+        }
+        TreeNode root = new TreeNode(rootVal);
+        dfs(root, descriptions);
+        return root;
+    }
+
+    public void dfs(TreeNode node, int[][] desc) {
+        if (node == null) {
+            return;
+        }
+        int[] childs = map.get(node.val);
+        if (childs != null && childs[0] != 0) {
+            node.left = new TreeNode(childs[0]);
+            dfs(node.left, desc);
+        }
+        if (childs != null && childs[1] != 0) {
+            node.right = new TreeNode(childs[1]);
+            dfs(node.right, desc);
+        }
+    }
+
+    public void createMap(int[][] desc) {
+        for (int[] d: desc) {
+            int k = d[0];
+            int[] key = new int[2];
+            if (map.containsKey(k)) {
+                key = map.get(k);
+            }
+            if (d[2] == 1) {
+                key[0] = d[1];
+            } else {
+                key[1] = d[1];
+            }
+            map.put(k, key);
+        }
+    }
+}
