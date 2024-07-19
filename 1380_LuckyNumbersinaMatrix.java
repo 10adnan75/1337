d1d83d60eb3b31173ff84e82da09ec6273125735
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-07-19 17:03:42 +0530

    Update 1380_LuckyNumbersinaMatrix.java

--- 1380_LuckyNumbersinaMatrix.java
+++ 1380_LuckyNumbersinaMatrix.java
@@ -21,3 +21,32 @@ class Solution {
         return ans;
     }
 }
+
+// July 19, 2024
+class Solution {
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        Set<Integer> mins = new HashSet<>();
+        for (int[] row: matrix) {
+            mins.add(getMin(row));
+        }
+        for (int i=0; i<matrix[0].length; i++) {
+            int max = 0;
+            for (int j=0; j<matrix.length; j++) {
+                max = Math.max(max, matrix[j][i]);
+            }
+            if (mins.contains(max)) {
+                ans.add(max);
+            }
+        }
+        return ans;
+    }
+
+    public int getMin(int[] a) {
+        int min = Integer.MAX_VALUE;
+        for (int i: a) {
+            min = Math.min(min, i);
+        }
+        return min;
+    }
+}
