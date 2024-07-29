d7f3df2abb838f599ec73965463e72c6cff3212c
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-07-29 19:17:01 +0530

    Update 1395_CountNumberofTeams.java

--- 1395_CountNumberofTeams.java
+++ 1395_CountNumberofTeams.java
@@ -29,3 +29,23 @@ class Solution {
         return getTeams(rating) + getTeams(getReverse(rating));
     }
 }
+
+// Jul 29, 2024
+class Solution {
+    public int numTeams(int[] rating) {
+        int teams = 0;
+        for (int i=0; i<rating.length; i++) {
+            for (int j=i+1; j<rating.length; j++) {
+                for (int k=j+1; k<rating.length; k++) {
+                    if (rating[i] > rating[j] && rating[j] > rating[k]) {
+                        teams++;
+                    }
+                    if (rating[i] < rating[j] && rating[j] < rating[k]) {
+                        teams++;
+                    }
+                }
+            }
+        }
+        return teams;
+    }
+}
