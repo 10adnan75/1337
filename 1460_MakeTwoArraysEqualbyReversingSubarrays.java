5397c1bc2e964d6f31365e0d00e8afd8b6fd4629
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-08-04 01:29:39 +0530

    Update 1460_MakeTwoArraysEqualbyReversingSubarrays.java

--- 1460_MakeTwoArraysEqualbyReversingSubarrays.java
+++ 1460_MakeTwoArraysEqualbyReversingSubarrays.java
@@ -49,3 +49,22 @@ class Solution {
         return true;
     }
 }
+
+// Aug 4, 2024
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        int[] freq = new int[1001];
+        for (int i: target) {
+            freq[i]++;
+        }
+        for (int i: arr) {
+            freq[i]--;
+        }
+        for (int i: freq) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
