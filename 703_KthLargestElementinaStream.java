d81024649d842b5c9ae7fe58e91a788b3303dc8e
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-08-16 23:22:08 -0700

    Update 703_KthLargestElementinaStream.java

--- 703_KthLargestElementinaStream.java
+++ 703_KthLargestElementinaStream.java
@@ -1,3 +1,4 @@
+// TLE :)
 class KthLargest {
     private PriorityQueue<Integer> pq;
     private int K;
@@ -26,6 +27,53 @@ class KthLargest {
         }
         this.pq = newPQ;
         return max;
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
+
+// Accepted :)
+class KthLargest {
+    private PriorityQueue<Integer> pq;
+    private int K;
+
+    public KthLargest(int k, int[] nums) {
+        this.pq = new PriorityQueue<>();
+        this.K = k;
+        Arrays.sort(nums);
+        int i = 0, n = nums.length - 1;
+        while (n >= 0) {
+            if (i++ == k) {
+                break;
+            }
+            this.pq.offer(nums[n--]);
+        }
+    }
+    
+    public int add(int val) {
+        if (this.pq.isEmpty()) {
+            this.pq.offer(val);
+            return val;
+        }
+        if (this.pq.size() < this.K) {
+            this.pq.offer(val);
+            int k = this.pq.poll();
+            this.pq.offer(k);
+            return k;
+        }
+        int k = this.pq.poll();
+        if (val <= k) {
+            this.pq.offer(k);
+            return k;
+        }
+        this.pq.offer(val);
+        k = this.pq.poll();
+        this.pq.offer(k);
+        return k;
     }
 }
 
