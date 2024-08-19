28cc5f8f1198ee505556565732baf499ce7d2053
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-08-19 14:41:17 -0700

    Update 3258_CountSubstringsThatSatisfyK-ConstraintI.java

--- 3258_CountSubstringsThatSatisfyK-ConstraintI.java
+++ 3258_CountSubstringsThatSatisfyK-ConstraintI.java
@@ -1,9 +1,43 @@
+// Brute force
 class Solution {
     public int countKConstraintSubstrings(String s, int k) {
         int c = 0;
         for (int i=0; i<s.length(); i++) {
             for (int j=i+1; j<=s.length(); j++) {
                 if (check(s.substring(i, j), k)) {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+
+    public boolean check(String s, int k) {
+        int cZero = 0, cOne = 0;
+        for (char c: s.toCharArray()) {
+            if (c == '0') {
+                cZero++;
+            } else {
+                cOne++;
+            }
+        }
+        return cZero <= k || cOne <= k;
+    }
+}
+
+// Hash map
+class Solution {
+    public int countKConstraintSubstrings(String s, int k) {
+        Map<String, Boolean> map = new HashMap<>();
+        int c = 0;
+        String tmp = "";
+        for (int i=0; i<s.length(); i++) {
+            for (int j=i+1; j<=s.length(); j++) {
+                tmp = s.substring(i, j);
+                if (!map.containsKey(tmp)) {
+                    map.put(tmp, check(tmp, k));
+                }
+                if (map.get(tmp)) {
                     c++;
                 }
             }
