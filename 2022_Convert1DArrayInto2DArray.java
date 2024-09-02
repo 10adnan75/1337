21b1c45e891a788bf8bef51e1e19cd261090d3bf
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-09-01 21:01:23 -0700

    Update 2022_Convert1DArrayInto2DArray.java

--- 2022_Convert1DArrayInto2DArray.java
+++ 2022_Convert1DArrayInto2DArray.java
@@ -70,3 +70,20 @@ class Solution {
         return twoDArray;
     }
 }
+
+// Sep 1, 2024
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (m*n != original.length) {
+            return new int[][] {};
+        }
+        int[][] arr = new int[m][n];
+        int k = 0;
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                arr[i][j] = original[k++];
+            }
+        }
+        return arr;
+    }
+}
