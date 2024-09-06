d5acc1ac7f87accdd8f0aadffdc2210258442f54
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-09-05 22:09:09 -0700

    Update 3217_DeleteNodesFromLinkedListPresentinArray.java

--- 3217_DeleteNodesFromLinkedListPresentinArray.java
+++ 3217_DeleteNodesFromLinkedListPresentinArray.java
@@ -34,3 +34,38 @@ class Solution {
         return modifiedLL;
     }
 }
+
+// in-place
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        Set<Integer> set = new HashSet<>();
+        for (int n: nums) {
+            set.add(n);
+        }
+        ListNode temp = head;
+        while (temp != null && temp.next != null) {
+            while (temp != null && temp.next != null && set.contains(temp.next.val)) {
+                temp.next = temp.next.next;
+            }
+            temp = temp.next;
+        }
+        while (head != null) {
+            if (set.contains(head.val)) {
+                head = head.next;
+                continue;
+            }
+            break;
+        }
+        return head;
+    }
+}
