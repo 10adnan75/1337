7c2239439bcd3e27c58b7992c04d45f5a03999c9
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2024-09-17 10:25:14 -0700

    Update 884_UncommonWordsfromTwoSentences.java

--- 884_UncommonWordsfromTwoSentences.java
+++ 884_UncommonWordsfromTwoSentences.java
@@ -20,3 +20,28 @@ class Solution {
         return ans;
     }
 }
+
+// Sep 17, 2024
+class Solution {
+    public String[] uncommonFromSentences(String s1, String s2) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String s: s1.split(" ")) {
+            map.put(s, map.getOrDefault(s, 0)+1);
+        }
+        for (String s: s2.split(" ")) {
+            map.put(s, map.getOrDefault(s, 0)+1);
+        }
+        List<String> ans = new ArrayList<>();
+        for (Map.Entry e: map.entrySet()) {
+            if ((int)e.getValue() == 1) {
+                ans.add((String)e.getKey());
+            }
+        }
+        int i = 0;
+        String[] uncommon = new String[ans.size()];
+        for (String s: ans) {
+            uncommon[i++] = s;
+        }
+        return uncommon;
+    }
+}
