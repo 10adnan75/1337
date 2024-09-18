f818a1460ae03e869ec0f5cc0d9f194400e92ce4
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-09-18 13:41:39 -0700

    Update 7_ReverseInteger.java

--- 7_ReverseInteger.java
+++ 7_ReverseInteger.java
@@ -10,3 +10,45 @@ class Solution {
         return 0;
     }
 }
+
+// Sep 18, 2024
+class Solution {
+    public int reverse(int x) {
+        boolean neg = false;
+        long num = 0;
+        if (x < 0) {
+            neg = true;
+            x *= -1;
+        }
+        while (x > 0) {
+            int rem = x % 10;
+            num = (num*10) + rem;
+            x /= 10;
+        }
+        if (neg) {
+            num *= -1;
+        }
+        return num >= Integer.MIN_VALUE && num <= Integer.MAX_VALUE ? (int)num : 0;
+    }
+}
+
+// My code
+class Solution {
+    public int reverse(int x) {
+        boolean neg = false;
+        long num = 0;
+        if (x < 0) {
+            neg = true;
+            x *= -1;
+        }
+        while (x > 0) {
+            int rem = x % 10;
+            num = (num*10) + rem;
+            x /= 10;
+        }
+        if (neg) {
+            num *= -1;
+        }
+        return num >= -2147483648 && num <= 2147483647 ? (int)num : 0;
+    }
+}
