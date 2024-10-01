2da3852c2ed8d9e0919202144f18c5134e6f60ec
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-10-01 07:52:43 -0700

    Update 547_NumberofProvinces.java

--- 547_NumberofProvinces.java
+++ 547_NumberofProvinces.java
@@ -23,3 +23,34 @@ class Solution {
         return nums;
     }
 }
+
+// My code: Oct 1, 2024
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int i = 0, c = 0, n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        for (int[] node: isConnected) {
+            if (!visited[i]) {
+                visited[i] = true;
+                bfs(isConnected, n, i, visited);
+                c++;
+            }
+            i++;
+        }
+        return c;
+    }
+
+    public void bfs(int[][] mat, int n, int i, boolean[] visited) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            for (int j=0; j<n; j++) {
+                if (mat[curr][j] == 1 && !visited[j]) {
+                    visited[j] = true;
+                    q.add(j);
+                }
+            }
+        }
+    }
+}
