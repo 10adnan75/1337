42fc8e2fdf999daa14cf1dfa417a832962893433
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-10-26 14:19:17 -0700

    Update 303_RangeSumQuery-Immutable.java

--- 303_RangeSumQuery-Immutable.java
+++ 303_RangeSumQuery-Immutable.java
@@ -1,3 +1,4 @@
+// I used brute-force here, how silly of me?
 class NumArray {
     private int[] nums; 
 
@@ -14,6 +15,23 @@ class NumArray {
             sum += this.nums[i];
         }
         return sum;
+    }
+}
+
+// prefix-sum
+class NumArray {
+    private int[] prefixSum;
+
+    public NumArray(int[] nums) {
+        prefixSum = new int[nums.length];
+        prefixSum[0] = nums[0];
+        for (int i=1; i<nums.length; i++) {
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        return left == 0 ? prefixSum[right] : prefixSum[right] - prefixSum[left-1];
     }
 }
 
