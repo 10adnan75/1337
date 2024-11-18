d858f067ec8bb388cdcbfbed7ca16a5b66887830
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-11-17 19:32:42 -0800

    Update 1652_DefusetheBomb.java

--- 1652_DefusetheBomb.java
+++ 1652_DefusetheBomb.java
@@ -35,3 +35,34 @@ class Solution {
         }
     }
 }
+
+// November 17, 2024, USA, 7:30 PM (Pacific)
+// 0 ms 😎
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] decrypted = new int[n];
+
+        if (k == 0) {
+            return decrypted;
+        }
+        
+        for (int i=0; i<n; i++) {
+            decrypted[i] = cipher(code, i, k);
+        }
+
+        return decrypted;
+    }
+
+    public int cipher(int[] arr, int start, int diff) {
+        int sum = 0, k = Math.abs(diff), n = arr.length;
+        start = diff > 0 ? start+1 < n ? start+1 : 0 : start-1 >= 0 ? start-1 : n-1;
+
+        while (k-- > 0) {
+            sum += arr[start];
+            start = diff > 0 ? start+1 < n ? start+1 : 0 : start-1 >= 0 ? start-1 : n-1;
+        }
+        
+        return sum;
+    }
+}
