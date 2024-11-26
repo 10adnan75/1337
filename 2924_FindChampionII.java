7d77add29474150613542acce5234cad7049965c
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-11-25 21:51:16 -0800

    Update 2924_FindChampionII.java

--- 2924_FindChampionII.java
+++ 2924_FindChampionII.java
@@ -16,3 +16,22 @@ class Solution {
         return -1;
     }
 }
+
+// Beats 100% Java users :)
+class Solution {
+    public int findChampion(int n, int[][] edges) {
+        int champ = 0, i = 0, c = 0;
+        boolean[] indegree = new boolean[n];
+        for (int[] edge: edges) {
+            indegree[edge[1]] = true;;
+        }
+        for (boolean b: indegree) {
+            if (!b) {
+                champ = i;
+                c++;
+            }
+            i++;
+        }
+        return c == 1 ? champ : -1;
+    }
+}
