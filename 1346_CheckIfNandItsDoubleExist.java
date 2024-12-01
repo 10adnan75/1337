87b86c42d395f33266cdedc4e0aa9da2da2ed47b
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-12-01 00:42:10 -0800

    Update 1346_CheckIfNandItsDoubleExist.java

--- 1346_CheckIfNandItsDoubleExist.java
+++ 1346_CheckIfNandItsDoubleExist.java
@@ -55,3 +55,18 @@ class Solution {
         return false;
     }
 }
+
+// December 1, 2024, Sunday, 12:42 AM 
+// CA, USA
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for (int a: arr) {
+            if (set.contains(a*2) || (a%2==0 && set.contains(a/2))) {
+                return true;
+            }
+            set.add(a);
+        }
+        return false;
+    }
+}
