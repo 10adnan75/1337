c77ca51935cf800a016d4a70dbe934f6e64df9e9
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-12-04 10:19:27 -0800

    Update 2825_MakeStringaSubsequenceUsingCyclicIncrements.java

--- 2825_MakeStringaSubsequenceUsingCyclicIncrements.java
+++ 2825_MakeStringaSubsequenceUsingCyclicIncrements.java
@@ -25,3 +25,16 @@ class Solution {
 }
 
 // Accepted
+class Solution {
+    public boolean canMakeSubsequence(String str1, String str2) {
+        int i = 0, j = 0;
+        while (i < str1.length() && j < str2.length()) {
+            char c = str1.charAt(i), d = str2.charAt(j);
+            if (c == d || c+1 == d || (c == 'z' && d == 'a')) {
+                j++;
+            }
+            i++;
+        }
+        return j == str2.length();
+    }
+}
