c12f539fbd686577964dbd78342666f55e69741d
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-12-21 12:35:21 -0800

    Update 1431_KidsWiththeGreatestNumberofCandies.java

--- 1431_KidsWiththeGreatestNumberofCandies.java
+++ 1431_KidsWiththeGreatestNumberofCandies.java
@@ -1,3 +1,4 @@
+// IN
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int greatestNumber = 0;
@@ -9,5 +10,37 @@ class Solution {
             ans.add(extraCandies + candy >= greatestNumber);
         }
         return ans;
+    }
+}
+
+// US
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = getMax(candies);
+        List<Boolean> result = new ArrayList<>();
+
+        for (int candy : candies) {
+            if (candy + extraCandies >= max) {
+                result.add(true);
+            }
+            else {
+                result.add(false);
+            }
+        }
+
+        return result;
+    }
+
+    public int getMax(int[] candies) {
+        int max = Integer.MIN_VALUE;
+
+        for (int candy : candies) {
+            max = Math.max(max, candy);
+        }
+
+        return max;
     }
 }
