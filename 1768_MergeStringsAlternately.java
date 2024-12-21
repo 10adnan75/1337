8ae1030f21e203623d50803e522225f814dfa2ef
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-12-20 20:09:18 -0800

    Update 1768_MergeStringsAlternately.java

--- 1768_MergeStringsAlternately.java
+++ 1768_MergeStringsAlternately.java
@@ -1,3 +1,4 @@
+// India
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         StringBuilder sb = new StringBuilder();
@@ -11,5 +12,23 @@ class Solution {
             }
         }
         return min == word1.length() ? sb.append(word2.substring(min, word2.length())).toString() : sb.append(word1.substring(min, word1.length())).toString();
+    }
+}
+
+// United States
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0, j = 0;
+        while (i < word1.length() && j < word2.length()) {
+            sb.append(word1.charAt(i++)).append(word2.charAt(j++));
+        }
+        while (i < word1.length()) {
+            sb.append(word1.charAt(i++));
+        }
+        while (j < word2.length()) {
+            sb.append(word2.charAt(j++));
+        }
+        return sb.toString();
     }
 }
