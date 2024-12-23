6e1057325bda2d073aa9bf1aa3de2e4add1efe0b
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-12-23 13:57:57 -0800

    Update 345_ReverseVowelsofaString.java

--- 345_ReverseVowelsofaString.java
+++ 345_ReverseVowelsofaString.java
@@ -1,3 +1,4 @@
+// 6 ms, IN
 class Solution {
 
     public boolean isVowel(char c) {
@@ -30,5 +31,38 @@ class Solution {
             }
         }
         return ans.toString();
+    }
+}
+
+// 4 ms, US
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String reverseVowels(String s) {
+        StringBuilder sb = new StringBuilder();
+        List<Integer> indexes = new ArrayList<>();
+        char[] chars = s.toCharArray();
+        int i = 0;
+
+        for (char c : s.toCharArray()) {
+            if (isAVowel(c)) {
+                sb.append(c);
+                indexes.add(i);
+            }
+            i++;
+        }
+
+        i = indexes.size() - 1;
+
+        for (int index: indexes) {
+            chars[index] = sb.charAt(i--);
+        }
+
+        return new String(chars);
+    }
+
+    public boolean isAVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
 }
