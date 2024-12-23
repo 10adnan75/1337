8ff6e8fb2ccb6747944cd8d002265c646ddf54db
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-12-22 23:21:55 -0800

    Update 605_CanPlaceFlowers.java

--- 605_CanPlaceFlowers.java
+++ 605_CanPlaceFlowers.java
@@ -1,3 +1,4 @@
+// Masterpiece of a code written by me :)
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int countZero = 0;
@@ -21,5 +22,53 @@ class Solution {
             }
         }
         return n <= countZero;
+    }
+}
+
+// Need to improve!
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int length = flowerbed.length, i = 1;
+
+        if (length == 1 && flowerbed[0] == 0) {
+            return true;
+        }
+
+        if (length == 2) {
+            int countOfFlowers = 0;
+
+            if (n > 1) {
+                return false;
+            }
+
+            for (int flower: flowerbed) {
+                if (flower == 0) {
+                    countOfFlowers++;
+                }
+            }
+
+            return n == 0 || countOfFlowers == length;
+        }
+
+        if (flowerbed[0] == 0 && flowerbed[1] == 0) {
+            flowerbed[0] = 1;
+            n--;
+            i++;
+        }
+
+        while (i < length - 1) {
+            if (flowerbed[i] == 0 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1;
+                n--;
+                i++;
+            }
+            i++;
+        }
+
+        if (flowerbed[length - 1] == 0 && flowerbed[length - 2] == 0) {
+            n--;
+        }
+
+        return n <= 0;
     }
 }
