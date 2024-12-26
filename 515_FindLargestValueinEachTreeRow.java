51336fed1012761ccc77780d3a6e3e9057a25019
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-12-25 17:18:40 -0800

    Update 515_FindLargestValueinEachTreeRow.java

--- 515_FindLargestValueinEachTreeRow.java
+++ 515_FindLargestValueinEachTreeRow.java
@@ -39,3 +39,70 @@ class Solution {
         return ans;
     }
 }
+
+// Dec 25, 2024, US
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> largestValues(TreeNode root) {
+        Map<Integer, List<Integer>> levels = new HashMap<>();
+
+        dfs(root, 0, levels);
+
+        return getMaxAtEachLevel(levels);
+    }
+
+    public void dfs(TreeNode root, int level, Map<Integer, List<Integer>> levels) {
+        if (root == null) return;
+
+        int val = root.val;
+
+        if (levels.containsKey(level)) {
+            levels.get(level).add(root.val);
+        } else {
+            List<Integer> list = new ArrayList<>();
+            list.add(val);
+            levels.put(level, list);
+        }
+
+        dfs(root.left, level + 1, levels);
+        dfs(root.right, level + 1, levels);
+    }
+
+    private List<Integer> getMaxAtEachLevel(Map<Integer, List<Integer>> levels) {
+        List<Integer> result = new ArrayList<>();
+
+        for (Map.Entry<Integer, List<Integer>> entry : levels.entrySet()) {
+            List<Integer> level = entry.getValue();
+            result.add(getMax(level));
+        }
+
+        return result;
+    }
+
+    private int getMax(List<Integer> level) {
+        int max = level.getFirst();
+
+        for (int i = 1; i < level.size(); i++) {
+            max = Math.max(max, level.get(i));
+        }
+
+        return max;
+    }
+}
