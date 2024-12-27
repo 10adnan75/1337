d9c431d1972f598889435d56421562c95afc929e
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2024-12-26 20:18:46 -0800

    Update 283_MoveZeroes.java

--- 283_MoveZeroes.java
+++ 283_MoveZeroes.java
@@ -42,3 +42,34 @@ class Solution {
         }
     }
 }
+
+// I solved this problem using another approach while in USA while listening to "Abhi na jao chodkar" by the legendary Mohammad Rafi :)
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0, j = 0, n = nums.length;
+
+        while (i < n && nums[i] != 0) {
+            i++;
+            j++;
+        }
+
+        while (i < n) {
+            if (nums[i] != 0) {
+                while (j < n && nums[j] != 0) {
+                    j++;
+                }
+
+                if (j < n ) {
+                    swap(nums, i, j);
+                }
+            }
+            i++;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
