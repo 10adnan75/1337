00eea6b3a45396e237a38902ec1462eb71cf2110
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2025-02-18 19:02:00 -0800

    Update 392_IsSubsequence.java

--- 392_IsSubsequence.java
+++ 392_IsSubsequence.java
@@ -18,3 +18,26 @@ class Solution {
         return false;
     }
 }
+
+// Feb 18, 2025, 7:00 PM, US
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sPointer = 0; 
+        int tPointer = 0;
+        int sLen = s.length();
+        int tLen = t.length();
+
+        while (sPointer < sLen && tPointer < tLen) {
+            char sChar = s.charAt(sPointer);
+            char tChar = t.charAt(tPointer);
+
+            if (sChar == tChar) {
+                sPointer++;
+            }
+
+            tPointer++;
+        }
+
+        return sPointer == sLen;
+    }
+}
