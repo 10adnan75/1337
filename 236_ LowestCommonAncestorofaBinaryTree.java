5743035804d6d9a64179cc902c426492fde154b7
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2025-02-26 14:14:32 -0800

    Update 236_ LowestCommonAncestorofaBinaryTree.java

--- 236_ LowestCommonAncestorofaBinaryTree.java
+++ 236_ LowestCommonAncestorofaBinaryTree.java
@@ -20,3 +20,39 @@ class Solution {
         return right != null ? right : left;
     }
 }
+
+// My code
+// Feb 26, 2025, 2:14 PM, US
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // Base case
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        // Left child
+        TreeNode leftChild = lowestCommonAncestor(root.left, p, q);
+
+        // Right child
+        TreeNode rightChild = lowestCommonAncestor(root.right, p, q);
+
+        if (leftChild == null) {
+            return rightChild;
+        }
+
+        if (rightChild == null) {
+            return leftChild;
+        }
+
+        return root;
+    }
+}
