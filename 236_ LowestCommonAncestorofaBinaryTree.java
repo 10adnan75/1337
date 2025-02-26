bcf6b5f9e2ef2c2c87a07d3f2030c406d9ae2c6b
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2025-02-26 14:49:10 -0800

    Update 236_ LowestCommonAncestorofaBinaryTree.java

--- 236_ LowestCommonAncestorofaBinaryTree.java
+++ 236_ LowestCommonAncestorofaBinaryTree.java
@@ -56,3 +56,76 @@ class Solution {
         return root;
     }
 }
+
+// Brute Force
+// Feb 26, 2025, 2:42 PM, US
+/*
+        1
+       /  \
+      2    3
+     / \    \
+    4   5    6
+
+lca(4,6): 1
+    path from root to 4: 1->2->4, path1[] = {1, 2, 4}
+    path from root to 6: 1->3->6, path2[] = {1, 3, 6}
+
+lca(4,5): 2
+    path from root to 4: 1->2->4, path1[] = {1, 2, 4}
+    path from root to 5: 1->2->5, path2[] = {1, 2, 5}
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> pathOfP = getPathFromRoot(root, p);
+        List<TreeNode> pathOfQ = getPathFromRoot(root, q);
+
+        TreeNode ans = root;
+
+        for (int i=0; i<Math.min(pathOfP.size(), pathOfQ.size()); i++) {
+            
+            if (pathOfP.get(i) != pathOfQ.get(i)) {
+                break;
+            }
+
+            ans = pathOfP.get(i);
+        }
+
+        return ans;
+    }
+
+    public List<TreeNode> getPathFromRoot(TreeNode root, TreeNode node) {
+        List<TreeNode> pathFromRoot = new ArrayList<>();
+        dfs(root, node, pathFromRoot);
+        return pathFromRoot;
+    }
+
+    public boolean dfs(TreeNode root, TreeNode node, List<TreeNode> path) {
+        if (root == null) {
+            return false;
+        } 
+
+        path.add(root);
+
+        if (root == node) {
+            return true;
+        }
+
+        if (dfs(root.left, node, path) || (dfs(root.right, node, path))) {
+            return true;
+        }
+
+        path.remove(path.size()-1);
+        return false;
+    }
+}
