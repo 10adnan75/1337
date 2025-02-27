eb01d88f602aa4140a84df4b6867680cadedf6b3
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2025-02-26 19:06:16 -0800

    Update 2_AddTwoNumbers.java

--- 2_AddTwoNumbers.java
+++ 2_AddTwoNumbers.java
@@ -74,3 +74,101 @@ class Solution {
         return dummy.next;
     }
 }
+
+// Brute force: not working for very long number!
+// WA: overflow, -ve output
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Long num1 = convertListToLong(l1);
+        Long num2 = convertListToLong(l2);
+
+        long sum = num1 + num2;
+
+        return sum == 0 ? new ListNode(0) : convertLongToList(sum);
+    }
+
+    public long convertListToLong(ListNode node) {
+        long num = 0L;
+        long power = 0;
+
+        while (node != null) {
+            num += Math.pow(10, power++) * node.val;
+            node = node.next;
+        }
+
+        return num;
+    }
+
+    public ListNode convertLongToList(long num) {
+        ListNode node = new ListNode();
+        ListNode temp = node;
+
+        while (num != 0) {
+            int n = (int) (num % 10);
+            temp.next = new ListNode(n);
+            num = num / 10;
+            temp = temp.next;
+        }
+
+        return node.next;
+    }
+}
+
+// Accepted
+// FEB 26, 2025, 7:00 PM, US
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode sum = new ListNode();
+        ListNode temp = sum;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int v1 = 0;
+            int v2 = 0;
+
+            if (l1 != null) {
+                v1 = l1.val;
+            }
+
+            if (l2 != null) {
+                v2 = l2.val;
+            }
+
+            int num = carry + v1 + v2;
+
+            temp.next = new ListNode(num%10);
+            carry = num / 10;
+            temp = temp.next;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        return sum.next;
+    }
+}
