594df2107921fd0192450a099c7817a5a11c60b2
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2025-02-26 16:31:55 -0800

    Update 21_MergeTwoSortedLists.java

--- 21_MergeTwoSortedLists.java
+++ 21_MergeTwoSortedLists.java
@@ -53,3 +53,48 @@ class Solution {
         return ans;
     }
 }
+
+// My code (Brute force)
+// feb 25, 2026, 4:30 PM, US
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+
+        if (list2 == null) {
+            return list1;
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        while (list1 != null) {
+            pq.add(list1.val);
+            list1 = list1.next;
+        }
+
+        while (list2 != null) {
+            pq.add(list2.val);
+            list2 = list2.next;
+        }
+
+        ListNode ans = new ListNode(pq.poll());
+        ListNode temp = ans;
+        while (!pq.isEmpty()) {
+            temp.next = new ListNode(pq.poll());
+            temp = temp.next;
+        }
+
+        return ans;
+    }
+}
