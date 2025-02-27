29210158994cda262a3464da09f889b0a97742f6
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2025-02-26 16:57:56 -0800

    Update 746_MinCostClimbingStairs.java

--- 746_MinCostClimbingStairs.java
+++ 746_MinCostClimbingStairs.java
@@ -17,3 +17,26 @@ class Solution {
         return Math.min(dp[0], dp[1]);
     }
 }
+
+// My code
+// FEB 26, 2025, 5:00 PM, US
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+
+        if (n <= 1) {
+            return 0;
+        }
+
+        int[] dp = new int[n];
+        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for (int i=2; i<n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+        }
+
+        return Math.min(dp[n-1], dp[n-2]);
+    }
+}
