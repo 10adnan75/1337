47254a39363512586076f2464a211822916d52d6
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2025-02-26 19:21:57 -0800

    Update 143_ReorderList.java

--- 143_ReorderList.java
+++ 143_ReorderList.java
@@ -98,3 +98,48 @@ class Solution {
         }
     }
 }
+
+// My code
+// FEB 26, 2025, 7:20 PM, US
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+
+        List<ListNode> nodes = new ArrayList<>();
+        ListNode temp = head;
+
+        while (temp != null) {
+            nodes.add(temp);
+            temp = temp.next;
+        }
+
+        int i = 0;
+        int j = nodes.size() - 1;
+
+        while (i < j) {
+            nodes.get(i).next = nodes.get(j);
+            i++;
+
+            if (i >= j) {
+                break;
+            }
+
+            nodes.get(j).next = nodes.get(i);
+            j--;
+        }
+
+        nodes.get(i).next = null;
+    }
+}
