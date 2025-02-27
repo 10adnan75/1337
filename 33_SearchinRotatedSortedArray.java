671e357457f407bb90c9ee33b557bad165e9dbd1
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2025-02-26 19:51:21 -0800

    Update 33_SearchinRotatedSortedArray.java

--- 33_SearchinRotatedSortedArray.java
+++ 33_SearchinRotatedSortedArray.java
@@ -23,3 +23,48 @@ class Solution {
         return -1;
     }
 }
+
+// Binary search
+// FEB 26, 2025, 8:00 PM, US
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int lo = 0;
+        int hi = n - 1;
+
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+
+            if (nums[mid] > nums[hi]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+
+        int pivotElement = lo;
+        int found = binarySearch(nums, target, 0, pivotElement - 1);
+
+        if (found != -1) {
+            return found;
+        }
+
+        return binarySearch(nums, target, pivotElement, n - 1);
+    }
+
+    public int binarySearch(int[] nums, int target, int lo, int hi) {
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
