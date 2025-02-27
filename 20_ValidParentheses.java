66331bb97a6cea736bd32663c0f059e659b549e3
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2025-02-26 17:24:35 -0800

    Update 20_ValidParentheses.java

--- 20_ValidParentheses.java
+++ 20_ValidParentheses.java
@@ -15,3 +15,41 @@ class Solution {
         return stack.isEmpty();
     }
 }
+
+// My code: FEB 25, 2026, 5:20 PM, US
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c: s.toCharArray()) {
+            if (isOpenBracket(c)) {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                
+                char popped = stack.pop();
+                if (popped == '(') {
+                    if (c != ')') {
+                        return false;
+                    }
+                } else if (popped == '{') {
+                    if (c != '}') {
+                        return false;
+                    }
+                } else {
+                    if (c != ']') {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return stack.empty();
+    }
+
+    public boolean isOpenBracket(char c) {
+        return c == '[' || c == '{' || c == '(';
+    }
+}
