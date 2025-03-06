74a5adc484f0064adc2a56397f824e04f6617c64
Adnan Mazharuddin Shaikh <addysk07@gmail.com>
2025-03-06 01:07:58 -0800

    Update 2965_FindMissingandRepeatedValues.java

--- 2965_FindMissingandRepeatedValues.java
+++ 2965_FindMissingandRepeatedValues.java
@@ -1,3 +1,4 @@
+// INDIA
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int[] freq = new int[grid.length*grid[0].length+1];
@@ -16,5 +17,32 @@ class Solution {
             }
         }
         return new int[]{repeated, missing};
+    }
+}
+
+// UNITED STATES
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int[] ans = new int[2];
+        Set<Integer> set = new HashSet<>();
+
+        for (int[] row: grid) {
+            for (int val: row) {
+                if (set.contains(val)) {
+                    ans[0] = val;
+                }
+
+                set.add(val);
+            }
+        }
+
+        for (int i=1; i<=Math.pow(grid.length, 2); i++) {
+            if (!set.contains(i)) {
+                ans[1] = i;
+                return ans;
+            }
+        }
+
+        return ans;
     }
 }
